c9393ac24ef77d36972c316f01a965510218a2ef
Aaron Kaye <kaye.aaron@gmail.com>
2023-05-23 15:28:24 -0400

    imports added to AddDeviceActivity

--- app/src/main/java/com/nashss/se/htmvault/activity/AddDeviceActivity.java
+++ app/src/main/java/com/nashss/se/htmvault/activity/AddDeviceActivity.java
@@ -1,5 +1,8 @@
 package com.nashss.se.htmvault.activity;
 
+import com.nashss.se.htmvault.activity.requests.AddDeviceRequest;
+import com.nashss.se.htmvault.activity.results.AddDeviceResult;
+import com.nashss.se.htmvault.dynamodb.DeviceDao;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
