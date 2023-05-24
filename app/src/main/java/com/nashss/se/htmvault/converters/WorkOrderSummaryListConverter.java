9117f9543b61ae13628e938b77966a1e107d447d
Aaron Kaye <kaye.aaron@gmail.com>
2023-05-24 10:46:29 -0400

    WorkOrderSummaryListConverter class implementation

--- app/src/main/java/com/nashss/se/htmvault/converters/WorkOrderSummaryListConverter.java
+++ app/src/main/java/com/nashss/se/htmvault/converters/WorkOrderSummaryListConverter.java
@@ -2,6 +2,7 @@ package com.nashss.se.htmvault.converters;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTypeConverter;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.nashss.se.htmvault.dynamodb.models.WorkOrderSummary;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -15,11 +16,11 @@ public class WorkOrderSummaryListConverter implements DynamoDBTypeConverter<Stri
 
     @Override
     public String convert(List<WorkOrderSummary> workOrderSummariesToSerialize) {
-        return null;
+        return GSON.toJson(workOrderSummariesToSerialize);
     }
 
     @Override
     public List<WorkOrderSummary> unconvert(String serializedListWorkOrderSummaries) {
-        return null;
+        return GSON.fromJson(serializedListWorkOrderSummaries, new TypeToken<List<WorkOrderSummary>>() { } .getType());
     }
 }
