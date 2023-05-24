2f93be0f15ead6eb3c23df96577c9974b00c66a5
Aaron Kaye <kaye.aaron@gmail.com>
2023-05-24 14:33:38 -0400

    updated LocalDateConverter & LocalDateTimeConverter to use format/parse methods to convert/unconvert, in place of Gson conversion

--- app/src/main/java/com/nashss/se/htmvault/converters/LocalDateConverter.java
+++ app/src/main/java/com/nashss/se/htmvault/converters/LocalDateConverter.java
@@ -1,26 +1,24 @@
 package com.nashss.se.htmvault.converters;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTypeConverter;
-import com.google.gson.Gson;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class LocalDateConverter implements DynamoDBTypeConverter<String, LocalDate> {
 
-    private static final Gson GSON = new Gson();
     private final Logger log = LogManager.getLogger();
-
 
     @Override
     public String convert(LocalDate localDateToSerialize) {
-        return GSON.toJson(localDateToSerialize);
+        return localDateToSerialize.format(DateTimeFormatter.ISO_LOCAL_DATE);
     }
 
     @Override
     public LocalDate unconvert(String serializedLocalDate) {
-        return GSON.fromJson(serializedLocalDate, LocalDate.class);
+        return LocalDate.parse(serializedLocalDate);
     }
 }
 
--- app/src/main/java/com/nashss/se/htmvault/converters/LocalDateTimeConverter.java
+++ app/src/main/java/com/nashss/se/htmvault/converters/LocalDateTimeConverter.java
@@ -1,25 +1,23 @@
 package com.nashss.se.htmvault.converters;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTypeConverter;
-import com.google.gson.Gson;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class LocalDateTimeConverter implements DynamoDBTypeConverter<String, LocalDateTime> {
 
-    private static final Gson GSON = new Gson();
     private final Logger log = LogManager.getLogger();
-
 
     @Override
     public String convert(LocalDateTime localDateTimeToSerialize) {
-        return GSON.toJson(localDateTimeToSerialize);
+        return localDateTimeToSerialize.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
     }
 
     @Override
     public LocalDateTime unconvert(String serializedLocalDateTime) {
-        return GSON.fromJson(serializedLocalDateTime, LocalDateTime.class);
+        return LocalDateTime.parse(serializedLocalDateTime);
     }
 }
