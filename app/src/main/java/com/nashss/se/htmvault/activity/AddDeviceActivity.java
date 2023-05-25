8751d9cb8d3b0698933f29c3066beb3c7e84927d
Aaron Kaye <kaye.aaron@gmail.com>
2023-05-25 16:58:06 -0400

    HTMVaultServiceUtils ifNotValidString implementation. AddDeviceActivity handleRequest implementation in progress.

--- app/src/main/java/com/nashss/se/htmvault/activity/AddDeviceActivity.java
+++ app/src/main/java/com/nashss/se/htmvault/activity/AddDeviceActivity.java
@@ -4,23 +4,26 @@ import com.nashss.se.htmvault.activity.requests.AddDeviceRequest;
 import com.nashss.se.htmvault.activity.results.AddDeviceResult;
 import com.nashss.se.htmvault.dynamodb.DeviceDao;
 import com.nashss.se.htmvault.exceptions.InvalidAttributeException;
+import com.nashss.se.htmvault.metrics.MetricsConstants;
+import com.nashss.se.htmvault.metrics.MetricsPublisher;
 import com.nashss.se.htmvault.utils.HTMVaultServiceUtils;
 import com.nashss.se.htmvault.utils.NullUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import javax.inject.Inject;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class AddDeviceActivity {
 
     private final DeviceDao deviceDao;
     private final Logger log = LogManager.getLogger();
+    private MetricsPublisher metricsPublisher;
 
     @Inject
-    public AddDeviceActivity(DeviceDao deviceDao) {
+    public AddDeviceActivity(DeviceDao deviceDao, MetricsPublisher metricsPublisher) {
         this.deviceDao = deviceDao;
+        this.metricsPublisher = metricsPublisher;
     }
 
     public AddDeviceResult handleRequest(final AddDeviceRequest addDeviceRequest) {
@@ -34,11 +37,33 @@ public class AddDeviceActivity {
         requiredRequestParameterValues.put("Facility Name", addDeviceRequest.getFacilityName());
         requiredRequestParameterValues.put("Assigned Department", addDeviceRequest.getAssignedDepartment());
 
-        // ensures required values were provided in request; if any were not, an InvalidAttributeException is thrown
-        NullUtils.ifNull((requiredRequestParameterValues));
+        try {
+            // ensures required values were provided in request; if any were not, an InvalidAttributeException is thrown
+            NullUtils.ifNull((requiredRequestParameterValues));
 
-        // ensures required values in request are not empty or blank; if any are, an InvalidAttributeException is thrown
-        HTMVaultServiceUtils.ifEmptyOrBlank(requiredRequestParameterValues);
+            // ensures required values in request are not empty or blank; if any are, an InvalidAttributeException is
+            // thrown
+            HTMVaultServiceUtils.ifEmptyOrBlank(requiredRequestParameterValues);
+
+            // ensures the value provided for the control number meets the requirement of containing alphanumeric
+            // characters only
+            HTMVaultServiceUtils.ifNotValidString("Control Number",
+                    requiredRequestParameterValues.get("Control Number"), List.of(Character::isLetterOrDigit));
+
+            // ensures the value provided for the serial number meets the requirement of containing alphanumeric
+            // character or dashes only
+            HTMVaultServiceUtils.ifNotValidString("Serial Number",
+                    requiredRequestParameterValues.get("Serial Number"),
+                    Arrays.asList(Character::isLetterOrDigit, character -> character.equals('-')));
+        } catch (InvalidAttributeException e) {
+            metricsPublisher.addCount(MetricsConstants.ADDDEVICE_INVALIDATTRIBUTEVALUE_COUNT, 1);
+            throw new InvalidAttributeException(e.getMessage());
+        }
+
+        metricsPublisher.addCount(MetricsConstants.ADDDEVICE_INVALIDATTRIBUTEVALUE_COUNT, 0);
+
+
+
 
 
     }
--- app/src/main/java/com/nashss/se/htmvault/utils/HTMVaultServiceUtils.java
+++ app/src/main/java/com/nashss/se/htmvault/utils/HTMVaultServiceUtils.java
@@ -2,7 +2,9 @@ package com.nashss.se.htmvault.utils;
 
 import com.nashss.se.htmvault.exceptions.InvalidAttributeException;
 
+import java.util.List;
 import java.util.Map;
+import java.util.function.Predicate;
 
 public class HTMVaultServiceUtils {
 
@@ -19,6 +21,39 @@ public class HTMVaultServiceUtils {
         for (Map.Entry<String, String> entry : requiredRequestParameterValues.entrySet()) {
             if (entry.getValue().isEmpty() || entry.getValue().isBlank()) {
                 throw new InvalidAttributeException(String.format("The %s must not be empty or blank", entry.getKey()));
+            }
+        }
+    }
+
+    /**
+     * Checks a string value to see if it contains only characters that meet at least one of the desired conditions
+     * (i.e. only alphanumeric characters, or only alphanumeric characters and dashes). If not, an
+     * InvalidAttributeException is thrown, specifying the attribute and the value provided.
+     * @param attributeName the name of the attribute being checked
+     * @param stringToCheck the value of the attribute that was provided
+     * @param conditions a list of Character conditions against which to check each character of the string
+     */
+    public static void ifNotValidString(String attributeName, String stringToCheck,
+                                        List<Predicate<Character>> conditions) {
+
+        // check each character of the string being
+        for (int i = 0; i < stringToCheck.length(); i++) {
+            // this is an invalid character unless proven otherwise
+            boolean validCharacter = false;
+            // check this character against the list of predicate conditions provided
+            for (Predicate<Character> condition : conditions) {
+                if (condition.test(stringToCheck.charAt((i)))) {
+                    // if this character meets one of the conditions, it's a valid character
+                    // and we can stop checking it
+                    validCharacter = true;
+                    break;
+                }
+            }
+
+            // if the character was not proven to be valid, we throw an exception
+            if (!validCharacter) {
+                throw new InvalidAttributeException(String.format("The %s provided (%s) contained invalid characters",
+                        attributeName, stringToCheck));
             }
         }
     }
