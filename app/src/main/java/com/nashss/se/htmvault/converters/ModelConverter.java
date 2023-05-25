7d2227ec1695e0a5f3100e2a5942cd01eeac32a1
Aaron Kaye <kaye.aaron@gmail.com>
2023-05-25 12:32:54 -0400

    ModelConverter toDeviceModel implementation in progress

--- app/src/main/java/com/nashss/se/htmvault/converters/ModelConverter.java
+++ app/src/main/java/com/nashss/se/htmvault/converters/ModelConverter.java
@@ -1,4 +1,29 @@
 package com.nashss.se.htmvault.converters;
 
+import com.nashss.se.htmvault.dynamodb.models.Device;
+import com.nashss.se.htmvault.models.DeviceModel;
+
 public class ModelConverter {
+
+    public DeviceModel toDeviceModel(Device device) {
+        DeviceModel.Builder builder = DeviceModel.builder()
+                .withControlNumber(device.getControlNumber())
+                .withSerialNumber(device.getSerialNumber())
+                .withManufacturer(device.getManufacturerModel().getManufacturer())
+                .withModel(device.getManufacturerModel().getModel())
+                .withManufactureDate(device.getManufactureDate() == null ? "" : device.getManufactureDate().toString())
+                .withServiceStatus(device.getServiceStatus().toString())
+                .withFacilityName(device.getFacilityName())
+                .withAssignedDepartment(device.getAssignedDepartment())
+                .withComplianceThroughDate(device.getComplianceThroughDate().toString())
+                .withLastPmCompletionDate(device.getLastPmCompletionDate().toString())
+                .withNextPmDueDate(device.getNextPmDueDate().toString())
+                .withMaintenanceFrequencyInMonths(device.getMaintenanceFrequencyInMonths())
+                .withInventoryAddDate(device.getInventoryAddDate().toString())
+                .withAddedById(device.getAddedById())
+                .withAddedByName(device.getAddedByName())
+                .withNotes(device.getNotes());
+
+
+    }
 }
