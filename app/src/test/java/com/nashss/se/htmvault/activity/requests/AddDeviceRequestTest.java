bef93fe7ae891a1bcf0ab08b242d8524c66e80ba
Aaron Kaye <kaye.aaron@gmail.com>
2023-05-26 12:32:12 -0400

    AddDeviceRequestTest created, implementation in progress

---
+++ app/src/test/java/com/nashss/se/htmvault/activity/requests/AddDeviceRequestTest.java
@@ -0,0 +1,73 @@
+package com.nashss.se.htmvault.activity.requests;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddDeviceRequestTest {
+
+    private String controlNumber;
+    private String serialNumber;
+    private String manufacturer;
+    private String model;
+    private String manufactureDate;
+    private String facilityName;
+    private String assignedDepartment;
+    private int maintenanceFrequencyInMonths;
+    private String notes;
+    private String customerId;
+    private String customerName;
+    private static final String ASSERT_EQUALS_FAILURE_MESSAGE = "Expected value set in request object to be what was " +
+            "passed to builder";
+
+    @BeforeEach
+    void setUp() {
+        controlNumber = "123";
+        serialNumber = "456";
+        manufacturer = "a manufacturer";
+        model = "a model";
+        manufactureDate = "2023-05-26";
+        facilityName = "a hospital";
+        assignedDepartment = "ER";
+        maintenanceFrequencyInMonths = 6;
+        notes = "some notes";
+        customerId = "227345";
+        customerName = "John Doe";
+    }
+
+    @Test
+    public void builder_withValuesForAllFields_returnsAddDeviceRequestProperlyBuilt() {
+        // GIVEN
+        // setup values
+
+        // WHEN
+        AddDeviceRequest addDeviceRequest = AddDeviceRequest.builder()
+                .withControlNumber(controlNumber)
+                .withSerialNumber(serialNumber)
+                .withManufacturer(manufacturer)
+                .withModel(model)
+                .withManufactureDate(manufactureDate)
+                .withFacilityName(facilityName)
+                .withAssignedDepartment(assignedDepartment)
+                .withMaintenanceFrequencyInMonths(maintenanceFrequencyInMonths)
+                .withNotes(notes)
+                .withCustomerId(customerId)
+                .withCustomerName(customerName)
+                .build();
+
+        // THEN
+        assertEquals(controlNumber, addDeviceRequest.getControlNumber(), ASSERT_EQUALS_FAILURE_MESSAGE);
+        assertEquals(serialNumber, addDeviceRequest.getSerialNumber(), ASSERT_EQUALS_FAILURE_MESSAGE);
+        assertEquals(manufacturer, addDeviceRequest.getManufacturer(), ASSERT_EQUALS_FAILURE_MESSAGE);
+        assertEquals(model, addDeviceRequest.getModel(), ASSERT_EQUALS_FAILURE_MESSAGE);
+        assertEquals(manufactureDate, addDeviceRequest.getManufactureDate(), ASSERT_EQUALS_FAILURE_MESSAGE);
+        assertEquals(facilityName, addDeviceRequest.getFacilityName(), ASSERT_EQUALS_FAILURE_MESSAGE);
+        assertEquals(assignedDepartment, addDeviceRequest.getAssignedDepartment(), ASSERT_EQUALS_FAILURE_MESSAGE);
+        assertEquals(maintenanceFrequencyInMonths, addDeviceRequest.getMaintenanceFrequencyInMonths(),
+                ASSERT_EQUALS_FAILURE_MESSAGE);
+        assertEquals(notes, addDeviceRequest.getNotes(), ASSERT_EQUALS_FAILURE_MESSAGE);
+        assertEquals(customerId, addDeviceRequest.getCustomerId(), ASSERT_EQUALS_FAILURE_MESSAGE);
+        assertEquals(customerName, addDeviceRequest.getCustomerName(), ASSERT_EQUALS_FAILURE_MESSAGE);
+    }
+}
