6263da4af2f7257acbe712f4c81e6269c0d926ff
Aaron Kaye <kaye.aaron@gmail.com>
2023-05-26 13:09:15 -0400

    AddDeviceRequestTest implementation in progress

--- app/src/test/java/com/nashss/se/htmvault/activity/requests/AddDeviceRequestTest.java
+++ app/src/test/java/com/nashss/se/htmvault/activity/requests/AddDeviceRequestTest.java
@@ -42,6 +42,7 @@ class AddDeviceRequestTest {
         // setup values
 
         // WHEN
+        // all request fields built
         AddDeviceRequest addDeviceRequest = AddDeviceRequest.builder()
                 .withControlNumber(controlNumber)
                 .withSerialNumber(serialNumber)
@@ -78,7 +79,7 @@ class AddDeviceRequestTest {
         // setup values
 
         // WHEN
-        // manufacture date and notes not included in build
+        // manufacture date and notes not included in build; all others included
         AddDeviceRequest addDeviceRequest = AddDeviceRequest.builder()
                 .withControlNumber(controlNumber)
                 .withSerialNumber(serialNumber)
@@ -97,12 +98,12 @@ class AddDeviceRequestTest {
         assertEquals(serialNumber, addDeviceRequest.getSerialNumber(), ASSERT_EQUALS_FAILURE_MESSAGE);
         assertEquals(manufacturer, addDeviceRequest.getManufacturer(), ASSERT_EQUALS_FAILURE_MESSAGE);
         assertEquals(model, addDeviceRequest.getModel(), ASSERT_EQUALS_FAILURE_MESSAGE);
-        assertNull(addDeviceRequest.getManufactureDate(), ASSERT_EQUALS_FAILURE_MESSAGE);
+        assertNull(addDeviceRequest.getManufactureDate(), "Expected value not built to be null");
         assertEquals(facilityName, addDeviceRequest.getFacilityName(), ASSERT_EQUALS_FAILURE_MESSAGE);
         assertEquals(assignedDepartment, addDeviceRequest.getAssignedDepartment(), ASSERT_EQUALS_FAILURE_MESSAGE);
         assertEquals(maintenanceFrequencyInMonths, addDeviceRequest.getMaintenanceFrequencyInMonths(),
                 ASSERT_EQUALS_FAILURE_MESSAGE);
-        assertNull(addDeviceRequest.getNotes(), ASSERT_EQUALS_FAILURE_MESSAGE);
+        assertNull(addDeviceRequest.getNotes(), "Expected value not built to be null");
         assertEquals(customerId, addDeviceRequest.getCustomerId(), ASSERT_EQUALS_FAILURE_MESSAGE);
         assertEquals(customerName, addDeviceRequest.getCustomerName(), ASSERT_EQUALS_FAILURE_MESSAGE);
     }
@@ -113,7 +114,7 @@ class AddDeviceRequestTest {
         // setup values
 
         // WHEN
-        // maintenanceFrequencyInMonths not included in build
+        // maintenanceFrequencyInMonths not included in build, all others included
         AddDeviceRequest addDeviceRequest = AddDeviceRequest.builder()
                 .withControlNumber(controlNumber)
                 .withSerialNumber(serialNumber)
@@ -144,5 +145,116 @@ class AddDeviceRequestTest {
         assertEquals(customerName, addDeviceRequest.getCustomerName(), ASSERT_EQUALS_FAILURE_MESSAGE);
     }
 
+    @Test
+    public void builder_nullPassedInForOneValueToBuilder_returnsAddDeviceRequestProperlyBuilt() {
+        // GIVEN
+        // setup values
 
+        // WHEN
+        // all values passed in, except null for notes
+        AddDeviceRequest addDeviceRequest = AddDeviceRequest.builder()
+                .withControlNumber(controlNumber)
+                .withSerialNumber(serialNumber)
+                .withManufacturer(manufacturer)
+                .withModel(model)
+                .withManufactureDate(manufactureDate)
+                .withFacilityName(facilityName)
+                .withAssignedDepartment(assignedDepartment)
+                .withMaintenanceFrequencyInMonths(maintenanceFrequencyInMonths)
+                .withNotes(null)
+                .withCustomerId(customerId)
+                .withCustomerName(customerName)
+                .build();
+
+        // THEN
+        assertEquals(controlNumber, addDeviceRequest.getControlNumber(), ASSERT_EQUALS_FAILURE_MESSAGE);
+        assertEquals(serialNumber, addDeviceRequest.getSerialNumber(), ASSERT_EQUALS_FAILURE_MESSAGE);
+        assertEquals(manufacturer, addDeviceRequest.getManufacturer(), ASSERT_EQUALS_FAILURE_MESSAGE);
+        assertEquals(model, addDeviceRequest.getModel(), ASSERT_EQUALS_FAILURE_MESSAGE);
+        assertEquals(manufactureDate, addDeviceRequest.getManufactureDate(), ASSERT_EQUALS_FAILURE_MESSAGE);
+        assertEquals(facilityName, addDeviceRequest.getFacilityName(), ASSERT_EQUALS_FAILURE_MESSAGE);
+        assertEquals(assignedDepartment, addDeviceRequest.getAssignedDepartment(), ASSERT_EQUALS_FAILURE_MESSAGE);
+        assertEquals(maintenanceFrequencyInMonths, addDeviceRequest.getMaintenanceFrequencyInMonths(),
+                ASSERT_EQUALS_FAILURE_MESSAGE);
+        assertNull(addDeviceRequest.getNotes(), "Expected value not built to be null");
+        assertEquals(customerId, addDeviceRequest.getCustomerId(), ASSERT_EQUALS_FAILURE_MESSAGE);
+        assertEquals(customerName, addDeviceRequest.getCustomerName(), ASSERT_EQUALS_FAILURE_MESSAGE);
+    }
+
+    @Test
+    public void toString_withValuesForAllFields_returnsExpectedString() {
+        // GIVEN
+        // setup values
+
+        // WHEN
+        // all values included in build
+        AddDeviceRequest addDeviceRequest = AddDeviceRequest.builder()
+                .withControlNumber(controlNumber)
+                .withSerialNumber(serialNumber)
+                .withManufacturer(manufacturer)
+                .withModel(model)
+                .withManufactureDate(manufactureDate)
+                .withFacilityName(facilityName)
+                .withAssignedDepartment(assignedDepartment)
+                .withMaintenanceFrequencyInMonths(maintenanceFrequencyInMonths)
+                .withNotes(notes)
+                .withCustomerId(customerId)
+                .withCustomerName(customerName)
+                .build();
+
+        String expectedString = "AddDeviceRequest{" +
+                "controlNumber='" + controlNumber + '\'' +
+                ", serialNumber='" + serialNumber + '\'' +
+                ", manufacturer='" + manufacturer + '\'' +
+                ", model='" + model + '\'' +
+                ", manufactureDate='" + manufactureDate + '\'' +
+                ", facilityName='" + facilityName + '\'' +
+                ", assignedDepartment='" + assignedDepartment + '\'' +
+                ", maintenanceFrequencyInMonths=" + maintenanceFrequencyInMonths +
+                ", notes='" + notes + '\'' +
+                ", customerId='" + customerId + '\'' +
+                ", customerName='" + customerName + '\'' +
+                '}';
+
+        // THEN
+        assertEquals(expectedString, addDeviceRequest.toString(), ASSERT_EQUALS_FAILURE_MESSAGE);
+    }
+
+    @Test
+    public void toString_withValuesForSomeFields_returnsExpectedString() {
+        // GIVEN
+        // setup values
+
+        // WHEN
+        // manufacture date and notes not included in build, all others included
+        AddDeviceRequest addDeviceRequest = AddDeviceRequest.builder()
+                .withControlNumber(controlNumber)
+                .withSerialNumber(serialNumber)
+                .withManufacturer(manufacturer)
+                .withModel(model)
+                .withFacilityName(facilityName)
+                .withAssignedDepartment(assignedDepartment)
+                .withMaintenanceFrequencyInMonths(maintenanceFrequencyInMonths)
+                .withCustomerId(customerId)
+                .withCustomerName(customerName)
+                .build();
+
+        String expectedString = "AddDeviceRequest{" +
+                "controlNumber='" + controlNumber + '\'' +
+                ", serialNumber='" + serialNumber + '\'' +
+                ", manufacturer='" + manufacturer + '\'' +
+                ", model='" + model + '\'' +
+                ", manufactureDate='" + "null" + '\'' +
+                ", facilityName='" + facilityName + '\'' +
+                ", assignedDepartment='" + assignedDepartment + '\'' +
+                ", maintenanceFrequencyInMonths=" + maintenanceFrequencyInMonths +
+                ", notes='" + "null" + '\'' +
+                ", customerId='" + customerId + '\'' +
+                ", customerName='" + customerName + '\'' +
+                '}';
+
+        // THEN
+        // those values should be "null" in the string; the ones that were built should match the value passed in
+        assertEquals(expectedString, addDeviceRequest.toString(), ASSERT_EQUALS_FAILURE_MESSAGE);
+    }
 }
