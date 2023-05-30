157c0afe8650133795a63419122f6ceec1e57a6d
Aaron Kaye <kaye.aaron@gmail.com>
2023-05-30 16:16:07 -0400

    NullUtilsTest implementation of unit tests

--- app/src/test/java/com/nashss/se/htmvault/utils/NullUtilsTest.java
+++ app/src/test/java/com/nashss/se/htmvault/utils/NullUtilsTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.HashMap;
 import java.util.Map;
 
+import static com.nashss.se.htmvault.utils.NullUtils.ifNotNull;
 import static com.nashss.se.htmvault.utils.NullUtils.ifNull;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -70,14 +71,74 @@ class NullUtilsTest {
     }
 
     @Test
-    void testIfNull1() {
+    void ifNull_nonNullStringProvidedWithValIfNull_returnsOriginalString() {
+        // GIVEN
+        String testString = "Hello";
+
+        // WHEN
+        String result = ifNull(testString, "The test string was empty");
+
+        // THEN
+        assertEquals(testString, result);
     }
 
     @Test
-    void ifNotNull() {
+    void ifNull_nullStringProvidedWithValIfNull_returnsExpectedValue() {
+        // GIVEN
+        String testString = null;
+
+        // WHEN
+        String result = ifNull(testString, "The test string was empty");
+
+        // THEN
+        assertEquals("The test string was empty", result);
     }
 
     @Test
-    void testIfNotNull() {
+    void ifNotNull_nonNullStringProvidedWithValIfNull_returnsExpectedValue() {
+        // GIVEN
+        String testString = "Hello";
+
+        // WHEN
+        String result = ifNotNull(testString, testString + " World!");
+
+        // THEN
+        assertEquals("Hello World!", result);
+    }
+
+    @Test
+    void ifNotNull_nullStringProvidedWithValIfNull_returnsExpectedValue() {
+        // GIVEN
+        String testString = null;
+
+        // WHEN
+        String result = ifNull(testString, "The test string was null");
+
+        // THEN
+        assertEquals("The test string was null", result);
+    }
+
+    @Test
+    void ifNotNull_nonNullStringProvidedWithSupplierIfNotNull_returnsExpectedValue() {
+        // GIVEN
+        String testString = "Hello";
+
+        // WHEN
+        String result = ifNotNull(testString, String::new);
+
+        // THEN
+        assertTrue(result.isEmpty());
+    }
+
+    @Test
+    void ifNotNull_nullStringProvidedWithSupplierIfNotNull_returnsExpectedValue() {
+        // GIVEN
+        String testString = null;
+
+        // WHEN
+        String result = ifNotNull(testString, String::new);
+
+        // THEN
+        assertNull(result);
     }
 }
