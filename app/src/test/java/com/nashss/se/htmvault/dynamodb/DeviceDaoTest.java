09b692c3ee25f79552b8f5e3d5458f11d93f2cbd
Aaron Kaye <kaye.aaron@gmail.com>
2023-05-31 11:00:44 -0400

    checkstyle corrections

--- app/src/test/java/com/nashss/se/htmvault/dynamodb/DeviceDaoTest.java
+++ app/src/test/java/com/nashss/se/htmvault/dynamodb/DeviceDaoTest.java
@@ -1,14 +1,15 @@
 package com.nashss.se.htmvault.dynamodb;
 
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.nashss.se.htmvault.dynamodb.models.Device;
 import com.nashss.se.htmvault.metrics.MetricsPublisher;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.verify;
 import static org.mockito.MockitoAnnotations.initMocks;
 
