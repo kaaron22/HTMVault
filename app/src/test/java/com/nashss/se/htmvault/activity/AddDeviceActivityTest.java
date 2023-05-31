f8fb5c39ee53319d9aa5af25a1259bfd4865582a
Aaron Kaye <kaye.aaron@gmail.com>
2023-05-31 12:56:23 -0400

    AddDeviceActivityTest implementation in progress

--- app/src/test/java/com/nashss/se/htmvault/activity/AddDeviceActivityTest.java
+++ app/src/test/java/com/nashss/se/htmvault/activity/AddDeviceActivityTest.java
@@ -8,7 +8,9 @@ import com.nashss.se.htmvault.dynamodb.ManufacturerModelDao;
 import com.nashss.se.htmvault.dynamodb.models.Device;
 import com.nashss.se.htmvault.dynamodb.models.FacilityDepartment;
 import com.nashss.se.htmvault.dynamodb.models.ManufacturerModel;
+import com.nashss.se.htmvault.exceptions.InvalidAttributeValueException;
 import com.nashss.se.htmvault.metrics.MetricsPublisher;
+import com.nashss.se.htmvault.models.DeviceModel;
 import com.nashss.se.htmvault.models.ServiceStatus;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -21,8 +23,7 @@ import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.mockito.MockitoAnnotations.openMocks;
 
 class AddDeviceActivityTest {
@@ -39,19 +40,19 @@ class AddDeviceActivityTest {
     private AddDeviceActivity addDeviceActivity;
 
     Device device = new Device();
-    private String controlNumber = "123";
-    private String serialNumber = "456";
-    private String manufacturer = "a manufacturer";
-    private String model = "a model";
-    private ManufacturerModel manufacturerModel = new ManufacturerModel();
-    private String manufactureDate = "2023-05-26";
-    private String facilityName = "a hospital";
-    private String assignedDepartment = "ER";
-    private FacilityDepartment facilityDepartment = new FacilityDepartment();
-    private int maintenanceFrequencyInMonths = 6;
-    private String notes = "some notes";
-    private String customerId = "227345";
-    private String customerName = "John Doe";
+    private final String controlNumber = "123";
+    private final String serialNumber = "456";
+    private final String manufacturer = "a manufacturer";
+    private final String model = "a model";
+    private final ManufacturerModel manufacturerModel = new ManufacturerModel();
+    private final String manufactureDate = "2023-05-26";
+    private final String facilityName = "a hospital";
+    private final String assignedDepartment = "ER";
+    private final FacilityDepartment facilityDepartment = new FacilityDepartment();
+    private final int maintenanceFrequencyInMonths = 6;
+    private final String notes = "some notes";
+    private final String customerId = "227345";
+    private final String customerName = "John Doe";
 
     @BeforeEach
     void setUp() {
@@ -101,9 +102,78 @@ class AddDeviceActivityTest {
 
         // WHEN
         AddDeviceResult addDeviceResult = addDeviceActivity.handleRequest(addDeviceRequest);
+        DeviceModel deviceModel = addDeviceResult.getDeviceModel();
 
         // THEN
-        assertNotNull(addDeviceResult.getDeviceModel().getControlNumber());
+        verify(deviceDao).saveDevice(any(Device.class));
+        assertNotNull(deviceModel.getControlNumber());
+        assertEquals(serialNumber, deviceModel.getSerialNumber());
+        assertEquals(manufacturer, deviceModel.getManufacturer());
+        assertEquals(model, deviceModel.getModel());
+        assertEquals(manufactureDate, deviceModel.getManufactureDate());
+        assertEquals(ServiceStatus.IN_SERVICE.toString(), deviceModel.getServiceStatus());
+        assertEquals(facilityName, deviceModel.getFacilityName());
+        assertEquals(assignedDepartment, deviceModel.getAssignedDepartment());
+        assertEquals("", deviceModel.getComplianceThroughDate());
+        assertEquals("", deviceModel.getLastPmCompletionDate());
+        assertEquals();
 
     }
+
+    @Test
+    public void handleRequest_withRequiredValueNull_throwsInvalidAttributeValueException() {
+        AddDeviceRequest addDeviceRequest = AddDeviceRequest.builder()
+                .withControlNumber(null)
+                .withSerialNumber(serialNumber)
+                .withManufacturer(manufacturer)
+                .withModel(model)
+                .withManufactureDate(manufactureDate)
+                .withFacilityName(facilityName)
+                .withAssignedDepartment(assignedDepartment)
+                .withMaintenanceFrequencyInMonths(maintenanceFrequencyInMonths)
+                .withNotes(notes)
+                .withCustomerId(customerId)
+                .withCustomerName(customerName)
+                .build();
+        when(manufacturerModelDao.getManufacturerModel(anyString(), anyString())).thenReturn(manufacturerModel);
+        when(facilityDepartmentDao.getFacilityDepartment(anyString(), anyString())).thenReturn(facilityDepartment);
+        when(deviceDao.saveDevice(any(Device.class))).thenReturn(device);
+
+        // WHEN & THEN
+        assertThrows(InvalidAttributeValueException.class, () ->
+                addDeviceActivity.handleRequest(addDeviceRequest),
+                "Expected a null value for control number to result in an InvalidAttributeValueException " +
+                        "thrown");
+    }
+
+    @Test
+    public void handleRequest_withOptionalValueNull_createsAndSavesDevice() {
+        AddDeviceRequest addDeviceRequest = AddDeviceRequest.builder()
+                .withControlNumber(controlNumber)
+                .withSerialNumber(serialNumber)
+                .withManufacturer(manufacturer)
+                .withModel(model)
+                .withManufactureDate(null)
+                .withFacilityName(facilityName)
+                .withAssignedDepartment(assignedDepartment)
+                .withMaintenanceFrequencyInMonths(maintenanceFrequencyInMonths)
+                .withNotes(notes)
+                .withCustomerId(customerId)
+                .withCustomerName(customerName)
+                .build();
+        when(manufacturerModelDao.getManufacturerModel(anyString(), anyString())).thenReturn(manufacturerModel);
+        when(facilityDepartmentDao.getFacilityDepartment(anyString(), anyString())).thenReturn(facilityDepartment);
+        when(deviceDao.saveDevice(any(Device.class))).thenReturn(device);
+
+        // WHEN
+        AddDeviceResult addDeviceResult = addDeviceActivity.handleRequest(addDeviceRequest);
+
+        // THEN
+
+        assertDoesNotThrow(() -> addDeviceActivity.handleRequest(addDeviceRequest),
+                "Expected a null value for an optional attribute to result in an " +
+                        "InvalidAttributeValueException thrown");
+    }
+
+
 }
