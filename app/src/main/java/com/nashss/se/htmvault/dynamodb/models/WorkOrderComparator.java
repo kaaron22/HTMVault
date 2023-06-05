656ea5cbdaf0c657b5b55d77cec9ce750cc58621
Aaron Kaye <kaye.aaron@gmail.com>
2023-06-05 12:19:20 -0400

    GetDeviceWorkOrders implementation in progress - WorkOrderComparator creation/implementation

--- app/src/main/java/com/nashss/se/htmvault/dynamodb/models/WorkOrderComparator.java
+++ app/src/main/java/com/nashss/se/htmvault/dynamodb/models/WorkOrderComparator.java
@@ -1,4 +1,15 @@
 package com.nashss.se.htmvault.dynamodb.models;
 
-public class WorkOrderComparator {
+import java.util.Comparator;
+
+public class WorkOrderComparator implements Comparator<WorkOrder> {
+    @Override
+    public int compare(WorkOrder o1, WorkOrder o2) {
+        return o1.getWorkOrderId().compareTo(o2.getWorkOrderId());
+    }
+
+    @Override
+    public Comparator<WorkOrder> reversed() {
+        return Comparator.super.reversed();
+    }
 }
