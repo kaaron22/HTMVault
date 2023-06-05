494d0bf8f3a731a255a2fe76d93dd1094319742e
Aaron Kaye <kaye.aaron@gmail.com>
2023-06-05 12:10:20 -0400

    GetDeviceWorkOrders endpoint implementation in progress

--- app/src/main/java/com/nashss/se/htmvault/activity/GetDeviceWorkOrdersActivity.java
+++ app/src/main/java/com/nashss/se/htmvault/activity/GetDeviceWorkOrdersActivity.java
@@ -1,4 +1,31 @@
 package com.nashss.se.htmvault.activity;
 
+import com.nashss.se.htmvault.activity.requests.GetDeviceWorkOrdersRequest;
+import com.nashss.se.htmvault.activity.results.GetDeviceWorkOrdersResult;
+import com.nashss.se.htmvault.dynamodb.DeviceDao;
+import com.nashss.se.htmvault.metrics.MetricsPublisher;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.inject.Inject;
+
 public class GetDeviceWorkOrdersActivity {
+
+    private final DeviceDao deviceDao;
+    private final Logger log = LogManager.getLogger();
+    private final MetricsPublisher metricsPublisher;
+
+    @Inject
+    public GetDeviceWorkOrdersActivity(DeviceDao deviceDao, MetricsPublisher metricsPublisher) {
+        this.deviceDao = deviceDao;
+        this.metricsPublisher = metricsPublisher;
+    }
+
+    public GetDeviceWorkOrdersResult handleRequest(final GetDeviceWorkOrdersRequest getDeviceWorkOrdersRequest) {
+        log.info("Received GetDeviceWorkOrdersRequest {}", getDeviceWorkOrdersRequest);
+
+        String controlNumber = getDeviceWorkOrdersRequest.getControlNumber();
+
+
+    }
 }
--- app/src/main/java/com/nashss/se/htmvault/lambda/GetDeviceWorkOrdersLambda.java
+++ app/src/main/java/com/nashss/se/htmvault/lambda/GetDeviceWorkOrdersLambda.java
@@ -4,13 +4,25 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.nashss.se.htmvault.activity.requests.GetDeviceWorkOrdersRequest;
 import com.nashss.se.htmvault.activity.results.GetDeviceWorkOrdersResult;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class GetDeviceWorkOrdersLambda
         extends LambdaActivityRunner<GetDeviceWorkOrdersRequest, GetDeviceWorkOrdersResult>
         implements RequestHandler<LambdaRequest<GetDeviceWorkOrdersRequest>, LambdaResponse> {
 
+    private final Logger log = LogManager.getLogger();
+
     @Override
     public LambdaResponse handleRequest(LambdaRequest<GetDeviceWorkOrdersRequest> input, Context context) {
-        return null;
+        log.info("handleRequest");
+        return super.runActivity(
+                () -> input.fromPath(path ->
+                        GetDeviceWorkOrdersRequest.builder()
+                                .withControlNumber(path.get("controlNumber"))
+                                .build()),
+                (request, serviceComponent) ->
+                        serviceComponent.provideGetDeviceWorkOrdersActivity().handleRequest(request)
+        );
     }
 }
