571754b3e366afb33b721c844483adb2d9a690dc
Aaron Kaye <kaye.aaron@gmail.com>
2023-06-06 11:02:00 -0400

    WOrkOrderIdComparatorTest creation and implementation of unit tests

---
+++ app/src/test/java/com/nashss/se/htmvault/dynamodb/models/WorkOrderIdComparatorTest.java
@@ -0,0 +1,43 @@
+package com.nashss.se.htmvault.dynamodb.models;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WorkOrderIdComparatorTest {
+
+    WorkOrderIdComparator workOrderIdComparator = new WorkOrderIdComparator();
+
+    @Test
+    void compare_nonEqualWorkOrderIds_returnsCorrectComparisonValue() {
+        // GIVEN
+        WorkOrder workOrder1 = new WorkOrder();
+        workOrder1.setWorkOrderId("1");
+
+        WorkOrder workOrder2 = new WorkOrder();
+        workOrder2.setWorkOrderId("2");
+
+        // WHEN
+        int comparison = workOrderIdComparator.compare(workOrder1, workOrder2);
+
+        // THEN
+        assertTrue(comparison < 0);
+    }
+
+    @Test
+
+    void compare_equalWorkOrderIds_returnsCorrectComparisonValue() {
+        // GIVEN
+        WorkOrder workOrder1 = new WorkOrder();
+        workOrder1.setWorkOrderId("3");
+
+        WorkOrder workOrder2 = new WorkOrder();
+        workOrder2.setWorkOrderId("3");
+
+        // WHEN
+        int comparison = workOrderIdComparator.compare(workOrder1, workOrder2);
+
+        // THEN
+        assertEquals(0, comparison);
+    }
+}
