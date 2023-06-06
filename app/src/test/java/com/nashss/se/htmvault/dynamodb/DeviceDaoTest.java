57bdc0f619d35a5f859eeae67442d445d1ac6a52
Aaron Kaye <kaye.aaron@gmail.com>
2023-06-06 11:12:34 -0400

    DeviceDaoTest unit test added for getDevice method

--- app/src/test/java/com/nashss/se/htmvault/dynamodb/DeviceDaoTest.java
+++ app/src/test/java/com/nashss/se/htmvault/dynamodb/DeviceDaoTest.java
@@ -1,6 +1,7 @@
 package com.nashss.se.htmvault.dynamodb;
 
 import com.nashss.se.htmvault.dynamodb.models.Device;
+import com.nashss.se.htmvault.metrics.MetricsConstants;
 import com.nashss.se.htmvault.metrics.MetricsPublisher;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
@@ -8,9 +9,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 class DeviceDaoTest {
@@ -39,5 +43,20 @@ class DeviceDaoTest {
         // THEN
         verify(dynamoDBMapper).save(device);
         assertEquals(device, result);
+    }
+
+    @Test
+    public void getDevice_deviceWithControlNumberExists_callsMapperWithPartitionKey() {
+        // GIVEN
+        String controlNumber = "123";
+        when(dynamoDBMapper.load(Mockito.eq(Device.class), eq(controlNumber))).thenReturn(new Device());
+
+        // WHEN
+        Device device = deviceDao.getDevice(controlNumber);
+
+        // THEN
+        assertNotNull(device);
+        verify(dynamoDBMapper).load(Device.class, controlNumber);
+        verify(metricsPublisher).addCount(MetricsConstants.GETDEVICE_DEVICENOTFOUND_COUNT, 0);
     }
 }
