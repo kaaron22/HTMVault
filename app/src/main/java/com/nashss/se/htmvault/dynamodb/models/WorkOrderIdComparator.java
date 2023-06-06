24b02da1f7f79d203ab23ee1e3790b4303a18415
Aaron Kaye <kaye.aaron@gmail.com>
2023-06-06 10:55:46 -0400

    Refactored/renamed WorkOrderComparator to WorkOrderIdComparator

--- app/src/main/java/com/nashss/se/htmvault/dynamodb/models/WorkOrderIdComparator.java
+++ app/src/main/java/com/nashss/se/htmvault/dynamodb/models/WorkOrderIdComparator.java
@@ -2,7 +2,7 @@ package com.nashss.se.htmvault.dynamodb.models;
 
 import java.util.Comparator;
 
-public class WorkOrderComparator implements Comparator<WorkOrder> {
+public class WorkOrderIdComparator implements Comparator<WorkOrder> {
     @Override
     public int compare(WorkOrder o1, WorkOrder o2) {
         return o1.getWorkOrderId().compareTo(o2.getWorkOrderId());
