d2056c2d5cfcccb1611cf468042e37de9c200edf
Aaron Kaye <kaye.aaron@gmail.com>
2023-06-07 15:52:22 -0400

    RetireDeviceActivity handleRequest implementation in progress

--- app/src/main/java/com/nashss/se/htmvault/activity/RetireDeviceActivity.java
+++ app/src/main/java/com/nashss/se/htmvault/activity/RetireDeviceActivity.java
@@ -4,23 +4,28 @@ import com.nashss.se.htmvault.activity.requests.RetireDeviceRequest;
 import com.nashss.se.htmvault.activity.results.RetireDeviceResult;
 import com.nashss.se.htmvault.dynamodb.DeviceDao;
 
+import com.nashss.se.htmvault.dynamodb.WorkOrderDao;
 import com.nashss.se.htmvault.dynamodb.models.Device;
+import com.nashss.se.htmvault.dynamodb.models.WorkOrder;
 import com.nashss.se.htmvault.metrics.MetricsPublisher;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import javax.inject.Inject;
+import java.util.List;
 
 
 public class RetireDeviceActivity {
 
     private final DeviceDao deviceDao;
+    private final WorkOrderDao workOrderDao;
     private final Logger log = LogManager.getLogger();
     private final MetricsPublisher metricsPublisher;
 
     @Inject
-    public RetireDeviceActivity(DeviceDao deviceDao, MetricsPublisher metricsPublisher) {
+    public RetireDeviceActivity(DeviceDao deviceDao, WorkOrderDao workOrderDao, MetricsPublisher metricsPublisher) {
         this.deviceDao = deviceDao;
+        this.workOrderDao = workOrderDao
         this.metricsPublisher = metricsPublisher;
     }
 
@@ -33,8 +38,10 @@ public class RetireDeviceActivity {
         Device device = deviceDao.getDevice(controlNumber);
 
         // get device's work orders, if any
+        List<WorkOrder> workOrders = workOrderDao.getWorkOrders(controlNumber);
 
         // ensure none of the work orders are still open (if so, they need to be completed/closed first)
+
 
         // if these conditions are met, we can proceed to perform a soft delete (update the service status to 'RETIRED')
 
