3eff0abb4484b2eb12d5552f8e11e1e6bf129eee
Aaron Kaye <kaye.aaron@gmail.com>
2023-06-07 15:07:36 -0400

    RetireDeviceRequest creation/implementation

---
+++ app/src/main/java/com/nashss/se/htmvault/activity/requests/RetireDeviceRequest.java
@@ -0,0 +1,39 @@
+package com.nashss.se.htmvault.activity.requests;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+
+@JsonDeserialize(builder = RetireDeviceRequest.Builder.class)
+public class RetireDeviceRequest {
+
+    private String controlNumber;
+
+    private RetireDeviceRequest(String controlNumber) {
+        this.controlNumber = controlNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "RetireDeviceRequest{" +
+                "controlNumber='" + controlNumber + '\'' +
+                '}';
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    @JsonPOJOBuilder
+    public static class Builder {
+        private String controlNumber;
+
+        public Builder withControlNumber(String controlNumber) {
+            this.controlNumber = controlNumber;
+            return this;
+        }
+
+        public RetireDeviceRequest build() {
+            return new RetireDeviceRequest(controlNumber);
+        }
+    }
+}
