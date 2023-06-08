3241bb4341679a5f7aa8a771614aeb9fdead4212
Aaron Kaye <kaye.aaron@gmail.com>
2023-06-08 13:15:40 -0400

    RetireDeviceActivityTest implementation in progress - added unit test for case of device eligible for retirement; added helper method to perform deep copy of device

--- app/src/test/java/com/nashss/se/htmvault/activity/RetireDeviceActivityTest.java
+++ app/src/test/java/com/nashss/se/htmvault/activity/RetireDeviceActivityTest.java
@@ -6,17 +6,27 @@ import com.nashss.se.htmvault.dynamodb.DeviceDao;
 import com.nashss.se.htmvault.dynamodb.WorkOrderDao;
 import com.nashss.se.htmvault.dynamodb.models.Device;
 import com.nashss.se.htmvault.dynamodb.models.ManufacturerModel;
+import com.nashss.se.htmvault.dynamodb.models.WorkOrder;
 import com.nashss.se.htmvault.exceptions.DeviceNotFoundException;
 import com.nashss.se.htmvault.exceptions.RetireDeviceWithOpenWorkOrdersException;
+import com.nashss.se.htmvault.metrics.MetricsConstants;
+import com.nashss.se.htmvault.metrics.MetricsPublisher;
+import com.nashss.se.htmvault.models.ServiceStatus;
+import com.nashss.se.htmvault.models.WorkOrderCompletionStatus;
 import com.nashss.se.htmvault.test.helper.DeviceTestHelper;
+import com.nashss.se.htmvault.test.helper.WorkOrderTestHelper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.openMocks;
 
@@ -26,6 +36,7 @@ class RetireDeviceActivityTest {
     private DeviceDao deviceDao;
     @Mock
     private WorkOrderDao workOrderDao;
+    @Mock
 
     @InjectMocks
     private RetireDeviceActivity retireDeviceActivity;
@@ -76,5 +87,75 @@ class RetireDeviceActivityTest {
                 retireDeviceActivity.handleRequest(retireDeviceRequest),
                 "Expected request to retire a device with open work order(s) to result in " +
                         "RetireDeviceWithOpenWorkOrdersException to be thrown");
+    }
+
+    @Test
+    public void handleRequest_deviceEligibleToBeRetired_softDeletesDeviceAndReturnsInResult() {
+        // GIVEN
+        // a mock device to return when our method under test mocks the call to look for it by control number
+        ManufacturerModel manufacturerModel = new ManufacturerModel();
+        manufacturerModel.setManufacturer("TestManufacturer");
+        manufacturerModel.setModel("TestModel");
+        manufacturerModel.setRequiredMaintenanceFrequencyInMonths(12);
+        Device device = DeviceTestHelper.generateActiveDevice(1, manufacturerModel,
+                "TestFacility", "TestDepartment");
+
+        // our final expected device (used to ensure the only thing that ultimately changed was the service status)
+        Device expectedDevice = copyDevice(device);
+        expectedDevice.setServiceStatus(ServiceStatus.RETIRED);
+
+        RetireDeviceRequest retireDeviceRequest = RetireDeviceRequest.builder()
+                .withControlNumber(device.getControlNumber())
+                .withCustomerId("an ID")
+                .withCustomerName("a name")
+                .build();
+        when(deviceDao.getDevice(anyString())).thenReturn(device);
+
+        // a list of all the work orders for the device with none open
+        List<WorkOrder> workOrders = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            WorkOrder workOrder = WorkOrderTestHelper.generateWorkOrder(i, device.getControlNumber(),
+                    "SN1", manufacturerModel, "TestFacility",
+                    "TestDepartment");
+            // our generator randomly sets the completion status, so we need ensure it's set to closed
+            workOrder.setWorkOrderCompletionStatus(WorkOrderCompletionStatus.CLOSED);
+            workOrders.add(workOrder);
+        }
+        when(workOrderDao.getWorkOrders("123")).thenReturn(workOrders);
+
+        // WHEN
+        RetireDeviceResult result = retireDeviceActivity.handleRequest(retireDeviceRequest);
+
+        // THEN
+        verify(deviceDao).saveDevice(device);
+        assertEquals("RETIRED", result.getDevice().getServiceStatus());
+
+        // verify no other device information was modified by our method under test
+        DeviceTestHelper.assertDeviceEqualsDeviceModel(expectedDevice, result.getDevice());
+    }
+
+    private Device copyDevice(Device device) {
+        Device deviceCopy = new Device();
+        deviceCopy.setControlNumber(device.getControlNumber());
+        deviceCopy.setSerialNumber(device.getSerialNumber());
+        ManufacturerModel manufacturerModel = new ManufacturerModel();
+        manufacturerModel.setManufacturer(device.getManufacturerModel().getManufacturer());
+        manufacturerModel.setModel(device.getManufacturerModel().getModel());
+        manufacturerModel.setRequiredMaintenanceFrequencyInMonths(device.getManufacturerModel()
+                .getRequiredMaintenanceFrequencyInMonths());
+        deviceCopy.setManufactureDate(device.getManufactureDate());
+        deviceCopy.setManufacturerModel(manufacturerModel);
+        deviceCopy.setServiceStatus(device.getServiceStatus());
+        deviceCopy.setFacilityName(device.getFacilityName());
+        deviceCopy.setAssignedDepartment(device.getAssignedDepartment());
+        deviceCopy.setComplianceThroughDate(device.getComplianceThroughDate());
+        deviceCopy.setLastPmCompletionDate(device.getLastPmCompletionDate());
+        deviceCopy.setNextPmDueDate(device.getNextPmDueDate());
+        deviceCopy.setInventoryAddDate(device.getInventoryAddDate());
+        deviceCopy.setAddedById(device.getAddedById());
+        deviceCopy.setAddedByName(device.getAddedByName());
+        deviceCopy.setNotes(device.getNotes());
+
+        return deviceCopy;
     }
 }
