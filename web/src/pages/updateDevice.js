0ed70da7bebd861e2bebb4b8d8a5c0c117d7af5c
Aaron Kaye <kaye.aaron@gmail.com>
2023-06-09 12:42:51 -0400

    updateDevice.js implementation in progress

--- web/src/pages/updateDevice.js
+++ web/src/pages/updateDevice.js
@@ -3,7 +3,29 @@ import Header from "../components/header";
 import BindingClass from "../util/bindingClass";
 import DataStore from "../util/DataStore";
 
+class UpdateDevice extends BindingClass {
+    constructor() {
+        super();
+        this.bindClassMethods(['clientLoaded', 'submit', 'mount', 'redirectToViewDevice'], this);
+        this.DataStore = new DataStore();
+        this.DataStore.addChangeListener(this.redirectToViewDevice);
+        this.header = new Header(this.DataStore);
+        console.log("update device constructor");
+    }
 
+    async clientLoaded() {
+        const urlParams = new URLSearchParams(window.location.search);
+        const deviceId = urlParams.get('controlNumber');
+        document.getElementById('control-number').innerText = "Loading...";
+        const device = await this.clientLoaded.getDevice(deviceId);
+        this.DataStore.set('device', device);
+    }
+
+    mount() {
+        document.getElementById('update-device-record').addEventListener('click', this.submit);
+    }
+
+}
 
 const main = async () => {
     const updateDevice = new UpdateDevice();
