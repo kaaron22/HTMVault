284e6048ab014a85a33d126092093a9bd2399d94
Aaron Kaye <kaye.aaron@gmail.com>
2023-06-10 16:59:07 -0400

    ReactivateDeviceActivity implementation

--- app/src/main/java/com/nashss/se/htmvault/activity/ReactivateDeviceActivity.java
+++ app/src/main/java/com/nashss/se/htmvault/activity/ReactivateDeviceActivity.java
@@ -2,8 +2,11 @@ package com.nashss.se.htmvault.activity;
 
 import com.nashss.se.htmvault.activity.requests.ReactivateDeviceRequest;
 import com.nashss.se.htmvault.activity.results.ReactivateDeviceResult;
+import com.nashss.se.htmvault.converters.ModelConverter;
 import com.nashss.se.htmvault.dynamodb.DeviceDao;
+import com.nashss.se.htmvault.dynamodb.models.Device;
 import com.nashss.se.htmvault.metrics.MetricsPublisher;
+import com.nashss.se.htmvault.models.ServiceStatus;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -24,6 +27,17 @@ public class ReactivateDeviceActivity {
     public ReactivateDeviceResult handleRequest(final ReactivateDeviceRequest reactivateDeviceRequest) {
         log.info("Received ReactivateDeviceRequest {}", reactivateDeviceRequest);
 
+        String controlNumber = reactivateDeviceRequest.getControlNumber();
 
+        // get device, if it exists
+        Device device = deviceDao.getDevice(controlNumber);
+
+        device.setServiceStatus(ServiceStatus.IN_SERVICE);
+
+        deviceDao.saveDevice(device);
+
+        return ReactivateDeviceResult.builder()
+                .withDeviceModel(new ModelConverter().toDeviceModel(device))
+                .build();
     }
 }
