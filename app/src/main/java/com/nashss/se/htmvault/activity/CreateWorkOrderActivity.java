e872f735320482ef1386492acfa6c0a8f4fcda83
Aaron Kaye <kaye.aaron@gmail.com>
2023-06-10 20:31:58 -0400

    CreateWorkOrderActivity implementation in progress

--- app/src/main/java/com/nashss/se/htmvault/activity/CreateWorkOrderActivity.java
+++ app/src/main/java/com/nashss/se/htmvault/activity/CreateWorkOrderActivity.java
@@ -7,12 +7,15 @@ import com.nashss.se.htmvault.metrics.MetricsPublisher;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import javax.inject.Inject;
+
 public class CreateWorkOrderActivity {
 
     private final WorkOrderDao workOrderDao;
     private final MetricsPublisher metricsPublisher;
     private final Logger log = LogManager.getLogger();
 
+    @Inject
     public CreateWorkOrderActivity(WorkOrderDao workOrderDao, MetricsPublisher metricsPublisher) {
         this.workOrderDao = workOrderDao;
         this.metricsPublisher = metricsPublisher;
@@ -21,7 +24,16 @@ public class CreateWorkOrderActivity {
     public CreateWorkOrderResult handleRequest(final CreateWorkOrderRequest createWorkOrderRequest) {
         log.info("Received CreateWorkOrderRequest {}", createWorkOrderRequest);
 
+        // verify the work order type is one of the types allowed
 
 
+        // verify the problem reported is not null or blank
+
+
+        // if the request passes validation, create the new work order, with initial values set,
+        // then save it to the database
+
+
+        // convert the work order, build and return the result with the work order model
     }
 }
