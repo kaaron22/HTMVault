9e291eeb8a105154ed97698b06e0d9429d251c3c
Aaron Kaye <kaye.aaron@gmail.com>
2023-06-12 12:19:15 -0400

    CreateWorkOrderActivityTest class creation

---
+++ app/src/test/java/com/nashss/se/htmvault/activity/CreateWorkOrderActivityTest.java
@@ -0,0 +1,12 @@
+package com.nashss.se.htmvault.activity;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CreateWorkOrderActivityTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
