7919d0ae0e8ade1771a4806aedbefebad809f4bb
Aaron Kaye <kaye.aaron@gmail.com>
2023-06-12 20:05:21 -0400

    GetWorkOrderRequest class creation, implementation

---
+++ app/src/main/java/com/nashss/se/htmvault/activity/requests/GetWorkOrderRequest.java
@@ -0,0 +1,38 @@
+package com.nashss.se.htmvault.activity.requests;
+
+public class GetWorkOrderRequest {
+
+    private final String workOrderId;
+
+    private GetWorkOrderRequest(String workOrderId) {
+        this.workOrderId = workOrderId;
+    }
+
+    public String getWorkOrderId() {
+        return workOrderId;
+    }
+
+    @Override
+    public String toString() {
+        return "GetWorkOrderRequest{" +
+                "workOrderId='" + workOrderId + '\'' +
+                '}';
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private String workOrderId;
+
+        public Builder withWorkOrderId(String workOrderId) {
+            this.workOrderId = workOrderId;
+            return this;
+        }
+
+        public GetWorkOrderRequest build() {
+            return new GetWorkOrderRequest(workOrderId);
+        }
+    }
+}
