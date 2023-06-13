7f6230c2cffef3200a18778cb77b3bd4dbdb3241
Aaron Kaye <kaye.aaron@gmail.com>
2023-06-12 21:02:31 -0400

    added inject dagger annotation to get work order activity constructor

--- app/src/main/java/com/nashss/se/htmvault/activity/GetWorkOrderActivity.java
+++ app/src/main/java/com/nashss/se/htmvault/activity/GetWorkOrderActivity.java
@@ -9,12 +9,15 @@ import com.nashss.se.htmvault.metrics.MetricsPublisher;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import javax.inject.Inject;
+
 public class GetWorkOrderActivity {
 
     private final WorkOrderDao workOrderDao;
     private final MetricsPublisher metricsPublisher;
     private final Logger log = LogManager.getLogger();
 
+    @Inject
     public GetWorkOrderActivity(WorkOrderDao workOrderDao, MetricsPublisher metricsPublisher) {
         this.workOrderDao = workOrderDao;
         this.metricsPublisher = metricsPublisher;
