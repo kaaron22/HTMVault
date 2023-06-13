402b2633954f0cbe3998423baa23c476b4b7dd85
Aaron Kaye <kaye.aaron@gmail.com>
2023-06-12 20:08:10 -0400

    GetWorkOrderResult class creation, implementation

---
+++ app/src/main/java/com/nashss/se/htmvault/activity/results/GetWorkOrderResult.java
@@ -0,0 +1,41 @@
+package com.nashss.se.htmvault.activity.results;
+
+import com.nashss.se.htmvault.models.WorkOrderModel;
+import com.nashss.se.htmvault.models.WorkOrderType;
+
+public class GetWorkOrderResult {
+
+    private final WorkOrderModel workOrder;
+
+    private GetWorkOrderResult(WorkOrderModel workOrder) {
+        this.workOrder = workOrder;
+    }
+
+    public WorkOrderModel getWorkOrder() {
+        return workOrder;
+    }
+
+    @Override
+    public String toString() {
+        return "GetWorkOrderResult{" +
+                "workOrder=" + workOrder +
+                '}';
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private WorkOrderModel workOrder;
+
+        public Builder withWorkOrderModel(WorkOrderModel workOrder) {
+            this.workOrder = workOrder;
+            return this;
+        }
+
+        public GetWorkOrderResult build() {
+            return new GetWorkOrderResult(workOrder);
+        }
+    }
+}
