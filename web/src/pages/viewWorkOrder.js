db876a8c5de8f9417464f34a6a4ce2f60a511654
Aaron Kaye <kaye.aaron@gmail.com>
2023-06-14 16:32:41 -0400

    added submit work order updates method to bind class methods list, updated client loaded method to hide update button if work order closed, removed duplicate statements to hide/unhide divs following work order updates

--- web/src/pages/viewWorkOrder.js
+++ web/src/pages/viewWorkOrder.js
@@ -6,7 +6,7 @@ import DataStore from "../util/DataStore";
 class ViewWorkOrder extends BindingClass {
     constructor() {
         super();
-        this.bindClassMethods(['clientLoaded', 'mount', 'addWorkOrderToPage', 'displayUpdateWorkOrderForm'], this);
+        this.bindClassMethods(['clientLoaded', 'mount', 'addWorkOrderToPage', 'displayUpdateWorkOrderForm', 'submitUpdatesWorkOrder'], this);
         this.dataStore = new DataStore();
         this.dataStore.addChangeListener(this.addWorkOrderToPage);
         this.header = new Header(this.dataStore);
@@ -83,11 +83,6 @@ class ViewWorkOrder extends BindingClass {
             successMessageDisplay.classList.add('hidden');
         }, 3500);
 
-        const deviceRecordDiv = document.getElementById('device-record-div');
-        const updateDeviceDiv = document.getElementById('update-device-div');
-        updateDeviceDiv.classList.add('hidden');
-        deviceRecordDiv.classList.remove('hidden');
-
         const workOrderDiv = document.getElementById('work-order-display-div');
         const updateWorkOrderDiv = document.getElementById('update-work-order-form-div');
         workOrderDiv.classList.remove('hidden');
@@ -152,8 +147,11 @@ class ViewWorkOrder extends BindingClass {
     async clientLoaded() {
         const urlParams = new URLSearchParams(window.location.search);
         const workOrderId = urlParams.get('workOrderId');
-        document.getElementById('work-order-id');
+        document.getElementById('work-order-id').innerText = "Loading Work Order...";
         const workOrder = await this.client.getWorkOrder(workOrderId);
+        if (workOrder.workOrderCompletionStatus == "CLOSED") {
+            document.getElementById('update-work-order').classList.add('hidden');
+        }
         this.dataStore.set('workOrder', workOrder);
     }
 
@@ -168,26 +166,27 @@ class ViewWorkOrder extends BindingClass {
         document.getElementById('control-number').innerText = workOrder.controlNumber;
         document.getElementById('serial-number').innerText = workOrder.serialNumber;
         document.getElementById('completion-status').innerText = workOrder.workOrderCompletionStatus;
-        document.getElementById('await-status').innerText = workOrder.workOrderAwaitStatus
-        document.getElementById('manufacturer').innerText = workOrder.manufacturer
-        document.getElementById('model').innerText = workOrder.model
-        document.getElementById('facility-name').innerText = workOrder.facilityName
-        document.getElementById('assigned-department').innerText = workOrder.assignedDepartment
-        document.getElementById('problem-reported').innerText = workOrder.problemReported
-        document.getElementById('problem-found').innerText = workOrder.problemFound
-        document.getElementById('added-by-id').innerText = workOrder.createdById
-        document.getElementById('added-by-name').innerText = workOrder.createdByName
-        document.getElementById('created').innerText = workOrder.creationDateTime
-        document.getElementById('closed-by-id').innerText = workOrder.closedById
-        document.getElementById('closed-by-name').innerText = workOrder.closedByName
-        document.getElementById('closed').innerText = workOrder.closedDateTime
-        document.getElementById('completed').innerText = workOrder.completionDateTime
-        document.getElementById('summary').innerText = workOrder.summary
+        document.getElementById('await-status').innerText = workOrder.workOrderAwaitStatus;
+        document.getElementById('manufacturer').innerText = workOrder.manufacturer;
+        document.getElementById('model').innerText = workOrder.model;
+        document.getElementById('facility-name').innerText = workOrder.facilityName;
+        document.getElementById('assigned-department').innerText = workOrder.assignedDepartment;
+        document.getElementById('problem-reported').innerText = workOrder.problemReported;
+        document.getElementById('problem-found').innerText = workOrder.problemFound;
+        document.getElementById('added-by-id').innerText = workOrder.createdById;
+        document.getElementById('added-by-name').innerText = workOrder.createdByName;
+        document.getElementById('created').innerText = workOrder.creationDateTime;
+        document.getElementById('closed-by-id').innerText = workOrder.closedById;
+        document.getElementById('closed-by-name').innerText = workOrder.closedByName;
+        document.getElementById('closed').innerText = workOrder.closedDateTime;
+        document.getElementById('completed').innerText = workOrder.completionDateTime;
+        document.getElementById('summary').innerText = workOrder.summary;
     }
 
     mount() {
         document.getElementById('update-work-order').addEventListener('click', this.displayUpdateWorkOrderForm);
         document.getElementById('submit-updates-work-order').addEventListener('click', this.submitUpdatesWorkOrder);
+
         this.header.addHeaderToPage();
 
         this.client = new HTMVaultClient();
