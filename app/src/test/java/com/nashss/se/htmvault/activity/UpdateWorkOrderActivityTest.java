d2ac8620cdbcca27afc66701c8508a5140ef4d0c
Aaron Kaye <kaye.aaron@gmail.com>
2023-06-14 18:59:39 -0400

    UpdateWorkOrderActivityTest class creation, implementation in progress

---
+++ app/src/test/java/com/nashss/se/htmvault/activity/UpdateWorkOrderActivityTest.java
@@ -0,0 +1,45 @@
+package com.nashss.se.htmvault.activity;
+
+import com.nashss.se.htmvault.activity.requests.UpdateWorkOrderRequest;
+import com.nashss.se.htmvault.dynamodb.WorkOrderDao;
+import com.nashss.se.htmvault.exceptions.InvalidAttributeValueException;
+import com.nashss.se.htmvault.metrics.MetricsPublisher;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.MockitoAnnotations.openMocks;
+
+class UpdateWorkOrderActivityTest {
+
+    @Mock
+    private WorkOrderDao workOrderDao;
+    @Mock
+    private MetricsPublisher metricsPublisher;
+
+    @InjectMocks
+    private UpdateWorkOrderActivity updateWorkOrderActivity;
+
+    @BeforeEach
+    void setUp() {
+        openMocks(this);
+    }
+
+    @Test
+    public void handleRequest_nullWorkOrderId_throwsInvalidAttributeValueException() {
+        // GIVEN
+        UpdateWorkOrderRequest updateWorkOrderRequest = UpdateWorkOrderRequest.builder()
+                .withWorkOrderId(null)
+                .build();
+
+        // WHEN & THEN
+        assertThrows(InvalidAttributeValueException.class, () ->
+                updateWorkOrderActivity.handleRequest(updateWorkOrderRequest),
+                "Expected request to update a work order with a null work order ID to result in an " +
+                        "InvalidAttributeValueException thrown");
+    }
+
+
+}
