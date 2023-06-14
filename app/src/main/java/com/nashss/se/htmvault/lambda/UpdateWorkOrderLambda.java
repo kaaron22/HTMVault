34f05ca48e5818a8c40e1c795d8bc326328c07e0
Aaron Kaye <kaye.aaron@gmail.com>
2023-06-14 11:17:02 -0400

    UpdateWorkOrderLambda class implementation in progress

--- app/src/main/java/com/nashss/se/htmvault/lambda/UpdateWorkOrderLambda.java
+++ app/src/main/java/com/nashss/se/htmvault/lambda/UpdateWorkOrderLambda.java
@@ -1,2 +1,47 @@
-package com.nashss.se.htmvault.lambda;public class UpdateWorkOrderLambda {
+package com.nashss.se.htmvault.lambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.nashss.se.htmvault.activity.requests.UpdateWorkOrderRequest;
+import com.nashss.se.htmvault.activity.results.UpdateWorkOrderResult;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class UpdateWorkOrderLambda
+        extends LambdaActivityRunner<UpdateWorkOrderRequest, UpdateWorkOrderResult>
+        implements RequestHandler<AuthenticatedLambdaRequest<UpdateWorkOrderRequest>, LambdaResponse> {
+
+    private final Logger log = LogManager.getLogger();
+
+    /**
+     * Handles a Lambda Function request
+     *
+     * @param input   The Lambda Function input
+     * @param context The Lambda execution environment context object.
+     * @return The Lambda Function output
+     */
+    @Override
+    public LambdaResponse handleRequest(AuthenticatedLambdaRequest<UpdateWorkOrderRequest> input, Context context) {
+        log.info("handleRequest");
+
+        return super.runActivity(
+                () -> {
+                    UpdateWorkOrderRequest unauthenticatedRequest = input.fromBody(UpdateWorkOrderRequest.class);
+                    return input.fromUserClaims(claims ->
+                            UpdateWorkOrderRequest.builder()
+                                    .withWorkOrderId(unauthenticatedRequest.getWorkOrderId())
+                                    .withWorkOrderType(unauthenticatedRequest.getWorkOrderType())
+                                    .withWorkOrderAwaitStatus(unauthenticatedRequest.getWorkOrderAwaitStatus())
+                                    .withProblemReported(unauthenticatedRequest.getProblemReported())
+                                    .withProblemFound(unauthenticatedRequest.getProblemFound())
+                                    .withSummary(unauthenticatedRequest.getSummary())
+                                    .withCompletionDateTime(unauthenticatedRequest.getCompletionDateTime())
+                                    .withCustomerId(claims.get("email"))
+                                    .withCustomerName(claims.get("name"))
+                                    .build(),
+                    (request, serviceComponent) -> serviceComponent
+                }
+        )
+
+    }
 }
