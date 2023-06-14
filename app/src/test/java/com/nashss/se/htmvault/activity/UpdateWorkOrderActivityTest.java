41df66379d56b3b85da7326966bfc91139b0b608
Aaron Kaye <kaye.aaron@gmail.com>
2023-06-14 19:30:51 -0400

    UpdateWorkOrderActivityTest implementation in progress

--- app/src/test/java/com/nashss/se/htmvault/activity/UpdateWorkOrderActivityTest.java
+++ app/src/test/java/com/nashss/se/htmvault/activity/UpdateWorkOrderActivityTest.java
@@ -2,10 +2,16 @@ package com.nashss.se.htmvault.activity;
 
 import com.nashss.se.htmvault.activity.requests.UpdateWorkOrderRequest;
 import com.nashss.se.htmvault.dynamodb.WorkOrderDao;
+import com.nashss.se.htmvault.dynamodb.models.ManufacturerModel;
+import com.nashss.se.htmvault.dynamodb.models.WorkOrder;
 import com.nashss.se.htmvault.exceptions.DeviceNotFoundException;
 import com.nashss.se.htmvault.exceptions.InvalidAttributeValueException;
+import com.nashss.se.htmvault.exceptions.UpdateClosedWorkOrderException;
 import com.nashss.se.htmvault.exceptions.WorkOrderNotFoundException;
+import com.nashss.se.htmvault.metrics.MetricsConstants;
 import com.nashss.se.htmvault.metrics.MetricsPublisher;
+import com.nashss.se.htmvault.models.WorkOrderCompletionStatus;
+import com.nashss.se.htmvault.test.helper.WorkOrderTestHelper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -13,6 +19,7 @@ import org.mockito.Mock;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.openMocks;
 
@@ -43,6 +50,7 @@ class UpdateWorkOrderActivityTest {
                 updateWorkOrderActivity.handleRequest(updateWorkOrderRequest),
                 "Expected request to update a work order with a null work order ID to result in an " +
                         "InvalidAttributeValueException thrown");
+        verify(metricsPublisher).addCount(MetricsConstants.UPDATEWORKORDER_INVALIDATTRIBUTEVALUE_COUNT, 1);
     }
 
     @Test
@@ -57,6 +65,7 @@ class UpdateWorkOrderActivityTest {
                         updateWorkOrderActivity.handleRequest(updateWorkOrderRequest),
                 "Expected request to update a work order with a null work order ID to result in an " +
                         "InvalidAttributeValueException thrown");
+        verify(metricsPublisher).addCount(MetricsConstants.UPDATEWORKORDER_INVALIDATTRIBUTEVALUE_COUNT, 1);
     }
 
     @Test
@@ -72,6 +81,29 @@ class UpdateWorkOrderActivityTest {
                 updateWorkOrderActivity.handleRequest(updateWorkOrderRequest),
                 "Expected request to update a work order for a work order not found to result in a " +
                         "WorkOrderNotFoundException thrown");
+        verify(metricsPublisher).addCount(MetricsConstants.UPDATEWORKORDER_WORKORDERNOTFOUND_COUNT, 1);
+    }
+
+    @Test
+    public void handleRequest_attemptUpdatesToClosedWorkOrder_throwsUpdateClosedWorkOrderException() {
+        // GIVEN
+        ManufacturerModel manufacturerModel = new ManufacturerModel();
+        manufacturerModel.setManufacturer("TestManufacturer");
+        manufacturerModel.setModel("TestModel");
+        manufacturerModel.setRequiredMaintenanceFrequencyInMonths(12);
+        WorkOrder workOrder = WorkOrderTestHelper.generateWorkOrder(1, "123",
+                "G321", manufacturerModel, "TestFacility", "TestDepartment");
+        workOrder.setWorkOrderCompletionStatus(WorkOrderCompletionStatus.CLOSED);
+        UpdateWorkOrderRequest updateWorkOrderRequest = UpdateWorkOrderRequest.builder()
+                .withWorkOrderId("NotFound")
+                .build();
+        when(workOrderDao.getWorkOrder(anyString())).thenReturn(workOrder);
+
+        // WHEN & THEN
+        assertThrows(UpdateClosedWorkOrderException.class, () ->
+                updateWorkOrderActivity.handleRequest(updateWorkOrderRequest),
+                "Expected request to update a closed work order to result in an " +
+                        "UpdateClosedWorkOrderException thrown");
     }
 
 
