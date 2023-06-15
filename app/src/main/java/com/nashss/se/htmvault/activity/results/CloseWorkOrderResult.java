e71a27ffaff3a54d28bbf08c1509668c83bc1cb5
Aaron Kaye <kaye.aaron@gmail.com>
2023-06-15 09:47:39 -0400

    CloseWorkOrderResult class creation, implementation

---
+++ app/src/main/java/com/nashss/se/htmvault/activity/results/CloseWorkOrderResult.java
@@ -0,0 +1,40 @@
+package com.nashss.se.htmvault.activity.results;
+
+import com.nashss.se.htmvault.models.WorkOrderModel;
+
+public class CloseWorkOrderResult {
+
+    private final WorkOrderModel workOrder;
+
+    private CloseWorkOrderResult(WorkOrderModel workOrder) {
+        this.workOrder = workOrder;
+    }
+
+    public WorkOrderModel getWorkOrder() {
+        return workOrder;
+    }
+
+    @Override
+    public String toString() {
+        return "CloseWorkOrderResult{" +
+                "workOrder=" + workOrder +
+                '}';
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private WorkOrderModel workOrder;
+
+        public Builder withWorkOrderModel(WorkOrderModel workOrder) {
+            this.workOrder = workOrder;
+            return this;
+        }
+
+        public CloseWorkOrderResult build() {
+            return new CloseWorkOrderResult(workOrder);
+        }
+    }
+}
