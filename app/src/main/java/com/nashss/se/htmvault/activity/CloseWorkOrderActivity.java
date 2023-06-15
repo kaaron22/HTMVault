95ced7ade24953c5fa25ba3c99ec850125861459
Aaron Kaye <kaye.aaron@gmail.com>
2023-06-15 12:15:28 -0400

    updated close work order activity to return existing work order as is if already closed (after converting and storing in result object)

--- app/src/main/java/com/nashss/se/htmvault/activity/CloseWorkOrderActivity.java
+++ app/src/main/java/com/nashss/se/htmvault/activity/CloseWorkOrderActivity.java
@@ -33,39 +33,43 @@ public class CloseWorkOrderActivity {
         // retrieve work order from database (work order not found exception is thrown by dao if applicable)
         WorkOrder workOrder = workOrderDao.getWorkOrder(closeWorkOrderRequest.getWorkOrderId());
 
-        // ensure the work order information has been filled in for fields that are optional while the work order is
-        // still open/ongoing, but which are required before the work order can be closed (i.e. completion date time
-        // must be set, there must be a summary filled in, etc.)
-        String problemFound = workOrder.getProblemFound();
-        String summary = workOrder.getSummary();
-        LocalDateTime completionDateTime = workOrder.getCompletionDateTime();
-        if (null == problemFound || problemFound.isBlank() || null == summary || summary.isBlank()
-                || null == completionDateTime) {
-            throw new CloseWorkOrderNotCompleteException("The work order information must be completed before " +
-                    "permanently closing " + workOrder.getWorkOrderId());
+        // if the work order is already closed, there's nothing to do except return the result with the converted
+        // work order
+        if (!(workOrder.getWorkOrderCompletionStatus() == WorkOrderCompletionStatus.CLOSED)) {
+            // ensure the work order information has been filled in for fields that are optional while the work order is
+            // still open/ongoing, but which are required before the work order can be closed (i.e. completion date time
+            // must be set, there must be a summary filled in, etc.)
+            String problemFound = workOrder.getProblemFound();
+            String summary = workOrder.getSummary();
+            LocalDateTime completionDateTime = workOrder.getCompletionDateTime();
+            if (null == problemFound || problemFound.isBlank() || null == summary || summary.isBlank()
+                    || null == completionDateTime) {
+                throw new CloseWorkOrderNotCompleteException("The work order information must be completed before " +
+                        "permanently closing " + workOrder.getWorkOrderId());
+            }
+
+            // proceed to update the work order as closed
+            // who closed it
+            workOrder.setClosedById(closeWorkOrderRequest.getCustomerId());
+            workOrder.setClosedByName(closeWorkOrderRequest.getCustomerName());
+
+            // the time closed (now)
+            LocalDateTime currentDateTime = LocalDateTime.now();
+            String currentDateTimeSerialized = new LocalDateTimeConverter().convert(currentDateTime);
+            LocalDateTime currentDateTimeNoNanos = new LocalDateTimeConverter().unconvert(currentDateTimeSerialized);
+            workOrder.setClosedDateTime(currentDateTimeNoNanos);
+
+            // await status no longer applicable
+            workOrder.setWorkOrderAwaitStatus(null);
+
+            // close the work order
+            workOrder.setWorkOrderCompletionStatus(WorkOrderCompletionStatus.CLOSED);
+
+            workOrder = workOrderDao.saveWorkOrder(workOrder);
         }
 
-        // proceed to update the work order as closed
-        // who closed it
-        workOrder.setClosedById(closeWorkOrderRequest.getCustomerId());
-        workOrder.setClosedByName(closeWorkOrderRequest.getCustomerName());
-
-        // the time closed (now)
-        LocalDateTime currentDateTime = LocalDateTime.now();
-        String currentDateTimeSerialized = new LocalDateTimeConverter().convert(currentDateTime);
-        LocalDateTime currentDateTimeNoNanos = new LocalDateTimeConverter().unconvert(currentDateTimeSerialized);
-        workOrder.setClosedDateTime(currentDateTimeNoNanos);
-
-        // await status no longer applicable
-        workOrder.setWorkOrderAwaitStatus(null);
-
-        // close the work order
-        workOrder.setWorkOrderCompletionStatus(WorkOrderCompletionStatus.CLOSED);
-
-        WorkOrder savedWorkOrder = workOrderDao.saveWorkOrder(workOrder);
-
         return CloseWorkOrderResult.builder()
-                .withWorkOrderModel(new ModelConverter().toWorkOrderModel(savedWorkOrder))
+                .withWorkOrderModel(new ModelConverter().toWorkOrderModel(workOrder))
                 .build();
     }
 }
