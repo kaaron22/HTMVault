687b104756ac4b603c7a5a3b58d114b7c9ba3850
Aaron Kaye <kaye.aaron@gmail.com>
2023-06-15 14:49:20 -0400

    CloseWorkOrderActivityTest class implementation in progress; private helper method added to defensively copy work order for comparison of result to expected

--- app/src/test/java/com/nashss/se/htmvault/activity/CloseWorkOrderActivityTest.java
+++ app/src/test/java/com/nashss/se/htmvault/activity/CloseWorkOrderActivityTest.java
@@ -1,9 +1,14 @@
 package com.nashss.se.htmvault.activity;
 
 import com.nashss.se.htmvault.activity.requests.CloseWorkOrderRequest;
+import com.nashss.se.htmvault.activity.results.CloseWorkOrderResult;
 import com.nashss.se.htmvault.dynamodb.WorkOrderDao;
+import com.nashss.se.htmvault.dynamodb.models.ManufacturerModel;
+import com.nashss.se.htmvault.dynamodb.models.WorkOrder;
 import com.nashss.se.htmvault.exceptions.WorkOrderNotFoundException;
 import com.nashss.se.htmvault.metrics.MetricsPublisher;
+import com.nashss.se.htmvault.models.WorkOrderCompletionStatus;
+import com.nashss.se.htmvault.test.helper.WorkOrderTestHelper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
@@ -41,5 +46,54 @@ class CloseWorkOrderActivityTest {
                 closeWorkOrderActivity.handleRequest(closeWorkOrderRequest),
                 "Expected a request to close a work order that was not found to result in a " +
                         "WorkOrderNotFoundException thrown");
+    }
+
+    @Test
+    public void handleRequest_workOrderAlreadyClosed_returnsWorkOrderInResult() {
+        // GIVEN
+        ManufacturerModel manufacturerModel = new ManufacturerModel();
+        manufacturerModel.setManufacturer("TestManufacturer");
+        manufacturerModel.setModel("TestModel");
+        manufacturerModel.setRequiredMaintenanceFrequencyInMonths(0);
+        WorkOrder workOrder = WorkOrderTestHelper.generateWorkOrder(1, "123",
+                "G321", manufacturerModel, "TestFacility", "TestDepartment");
+        workOrder.setWorkOrderCompletionStatus(WorkOrderCompletionStatus.CLOSED);
+        CloseWorkOrderRequest closeWorkOrderRequest = CloseWorkOrderRequest.builder()
+                .withWorkOrderId("a work order id")
+                .build();
+        when(workOrderDao.getWorkOrder(anyString())).thenReturn(workOrder);
+
+        WorkOrder expectedWorkOrder = copyWorkOrder(workOrder);
+
+        // WHEN
+        CloseWorkOrderResult closeWorkOrderResult = closeWorkOrderActivity.handleRequest(closeWorkOrderRequest);
+
+        // THEN
+        WorkOrderTestHelper.assertWorkOrderEqualsWorkOrderModel(expectedWorkOrder, closeWorkOrderResult.getWorkOrder());
+    }
+
+    private WorkOrder copyWorkOrder(WorkOrder workOrder) {
+        WorkOrder copyWorkOrder = new WorkOrder();
+        copyWorkOrder.setWorkOrderId(workOrder.getWorkOrderId());
+        copyWorkOrder.setWorkOrderType(workOrder.getWorkOrderType());
+        copyWorkOrder.setControlNumber(workOrder.getControlNumber());
+        copyWorkOrder.setSerialNumber(workOrder.getSerialNumber());
+        copyWorkOrder.setWorkOrderCompletionStatus(workOrder.getWorkOrderCompletionStatus());
+        copyWorkOrder.setWorkOrderAwaitStatus(workOrder.getWorkOrderAwaitStatus());
+        copyWorkOrder.setManufacturerModel(workOrder.getManufacturerModel());
+        copyWorkOrder.setFacilityName(workOrder.getFacilityName());
+        copyWorkOrder.setAssignedDepartment(workOrder.getAssignedDepartment());
+        copyWorkOrder.setProblemReported(workOrder.getProblemReported());
+        copyWorkOrder.setProblemFound(workOrder.getProblemFound());
+        copyWorkOrder.setCreatedById(workOrder.getCreatedById());
+        copyWorkOrder.setCreatedByName(workOrder.getCreatedByName());
+        copyWorkOrder.setCreationDateTime(workOrder.getCreationDateTime());
+        copyWorkOrder.setClosedById(workOrder.getClosedById());
+        copyWorkOrder.setClosedByName(workOrder.getClosedByName());
+        copyWorkOrder.setClosedDateTime(workOrder.getClosedDateTime());
+        copyWorkOrder.setSummary(workOrder.getSummary());
+        copyWorkOrder.setCompletionDateTime(workOrder.getCompletionDateTime());
+
+        return copyWorkOrder;
     }
 }
