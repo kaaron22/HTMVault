b13bf8c1021e49c0b70ea488430e01e74c6cf123
Aaron Kaye <kaye.aaron@gmail.com>
2023-06-16 13:34:05 -0400

    GetFacilitiesAndDepartmentsActivity implementation in progress

--- app/src/main/java/com/nashss/se/htmvault/activity/GetFacilitiesAndDepartmentsActivity.java
+++ app/src/main/java/com/nashss/se/htmvault/activity/GetFacilitiesAndDepartmentsActivity.java
@@ -1,2 +1,22 @@
-package com.nashss.se.htmvault.activity;public class GetFacilitiesAndDepartmentsActivity {
+package com.nashss.se.htmvault.activity;
+
+import com.nashss.se.htmvault.dynamodb.FacilityDepartmentDao;
+import com.nashss.se.htmvault.metrics.MetricsPublisher;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.inject.Inject;
+
+public class GetFacilitiesAndDepartmentsActivity {
+
+    private final FacilityDepartmentDao facilityDepartmentDao;
+    private final MetricsPublisher metricsPublisher;
+    private final Logger log = LogManager.getLogger();
+
+    @Inject
+    public GetFacilitiesAndDepartmentsActivity(FacilityDepartmentDao facilityDepartmentDao,
+                                               MetricsPublisher metricsPublisher) {
+        this.facilityDepartmentDao = facilityDepartmentDao;
+        this.metricsPublisher = metricsPublisher;
+    }
 }
