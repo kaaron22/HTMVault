19c9288be99865bf76f5d68c0688c834e327e604
Aaron Kaye <kaye.aaron@gmail.com>
2023-06-16 01:34:07 -0400

    updated model converter method to sort list of manufacturers and each corresponding list of models; added applicable comparator class

--- app/src/main/java/com/nashss/se/htmvault/converters/ModelConverter.java
+++ app/src/main/java/com/nashss/se/htmvault/converters/ModelConverter.java
@@ -2,6 +2,7 @@ package com.nashss.se.htmvault.converters;
 
 import com.nashss.se.htmvault.dynamodb.models.Device;
 import com.nashss.se.htmvault.dynamodb.models.ManufacturerModel;
+import com.nashss.se.htmvault.dynamodb.models.ManufacturerModelsComparator;
 import com.nashss.se.htmvault.dynamodb.models.WorkOrder;
 import com.nashss.se.htmvault.models.DeviceModel;
 import com.nashss.se.htmvault.models.ManufacturerModels;
@@ -9,10 +10,7 @@ import com.nashss.se.htmvault.models.WorkOrderModel;
 import com.nashss.se.htmvault.utils.CollectionUtils;
 import com.nashss.se.htmvault.utils.HTMVaultServiceUtils;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class ModelConverter {
 
@@ -90,12 +88,15 @@ public class ModelConverter {
     public List<ManufacturerModels> toListManufacturerModels(Map<String, Set<String>> manufacturersAndModels) {
         List<ManufacturerModels> manufacturerModelsList = new ArrayList<>();
         for (Map.Entry<String, Set<String>> entry : manufacturersAndModels.entrySet()) {
+            List<String> models = CollectionUtils.copyToList(entry.getValue());
+            Collections.sort(models);
             ManufacturerModels manufacturerModels = ManufacturerModels.builder()
                     .withManufacturer(entry.getKey())
-                    .withModels(CollectionUtils.copyToList(entry.getValue()))
+                    .withModels(models)
                     .build();
             manufacturerModelsList.add(manufacturerModels);
         }
+        manufacturerModelsList.sort(new ManufacturerModelsComparator());
         return manufacturerModelsList;
     }
 }
---
+++ app/src/test/java/com/nashss/se/htmvault/activity/GetManufacturersAndModelsActivityTest.java
@@ -0,0 +1,11 @@
+package com.nashss.se.htmvault.activity;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+class GetManufacturersAndModelsActivityTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
