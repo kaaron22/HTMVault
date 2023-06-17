834d395fbe2487b6810ff0b99877773b68c98022
Aaron Kaye <kaye.aaron@gmail.com>
2023-06-16 20:41:24 -0400

    removed unused imports

--- app/src/main/java/com/nashss/se/htmvault/activity/GetFacilitiesAndDepartmentsActivity.java
+++ app/src/main/java/com/nashss/se/htmvault/activity/GetFacilitiesAndDepartmentsActivity.java
@@ -1,13 +1,10 @@
 package com.nashss.se.htmvault.activity;
 
 import com.nashss.se.htmvault.activity.requests.GetFacilitiesAndDepartmentsRequest;
-import com.nashss.se.htmvault.activity.requests.GetManufacturersAndModelsRequest;
 import com.nashss.se.htmvault.activity.results.GetFacilitiesAndDepartmentsResult;
-import com.nashss.se.htmvault.activity.results.GetManufacturersAndModelsResult;
 import com.nashss.se.htmvault.converters.ModelConverter;
 import com.nashss.se.htmvault.dynamodb.FacilityDepartmentDao;
 import com.nashss.se.htmvault.dynamodb.models.FacilityDepartment;
-import com.nashss.se.htmvault.dynamodb.models.ManufacturerModel;
 import com.nashss.se.htmvault.metrics.MetricsPublisher;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
---
+++ app/src/test/java/com/nashss/se/htmvault/activity/GetFacilitiesAndDepartmentsActivityTest.java
@@ -0,0 +1,11 @@
+package com.nashss.se.htmvault.activity;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+class GetFacilitiesAndDepartmentsActivityTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
