c9572cb20ceb0e3d4684ce08312d31a8eb93a5e9
Aaron Kaye <kaye.aaron@gmail.com>
2023-06-19 16:30:22 -0400

    checkstyle corrections - ServiceComponent

--- app/src/main/java/com/nashss/se/htmvault/dependency/ServiceComponent.java
+++ app/src/main/java/com/nashss/se/htmvault/dependency/ServiceComponent.java
@@ -18,99 +18,96 @@ import dagger.Component;
 
 import javax.inject.Singleton;
 
-/**
- * The interface Service component.
- */
 @Singleton
 @Component(modules = {DaoModule.class, MetricsModule.class})
 public interface ServiceComponent {
 
     /**
-     * Provide add device activity add device activity.
+     * Provides add device activity.
      *
      * @return the add device activity
      */
     AddDeviceActivity provideAddDeviceActivity();
 
     /**
-     * Provide get device activity get device activity.
+     * Provides get device activity.
      *
      * @return the get device activity
      */
     GetDeviceActivity provideGetDeviceActivity();
 
     /**
-     * Provide get device work orders activity get device work orders activity.
+     * Provides get device work orders activity.
      *
      * @return the get device work orders activity
      */
     GetDeviceWorkOrdersActivity provideGetDeviceWorkOrdersActivity();
 
     /**
-     * Provide retire device activity retire device activity.
+     * Provides retire device activity.
      *
      * @return the retire device activity
      */
     RetireDeviceActivity provideRetireDeviceActivity();
 
     /**
-     * Provide update device activity update device activity.
+     * Provides update device activity.
      *
      * @return the update device activity
      */
     UpdateDeviceActivity provideUpdateDeviceActivity();
 
     /**
-     * Provide search devices activity search devices activity.
+     * Provides ssearch devices activity.
      *
      * @return the search devices activity
      */
     SearchDevicesActivity provideSearchDevicesActivity();
 
     /**
-     * Provide reactivate device activity reactivate device activity.
+     * Provides reactivate device activity.
      *
      * @return the reactivate device activity
      */
     ReactivateDeviceActivity provideReactivateDeviceActivity();
 
     /**
-     * Provide create work order activity create work order activity.
+     * Provides create work order activity.
      *
      * @return the create work order activity
      */
     CreateWorkOrderActivity provideCreateWorkOrderActivity();
 
     /**
-     * Provide get work order activity get work order activity.
+     * Provides get work order activity.
      *
      * @return the get work order activity
      */
     GetWorkOrderActivity provideGetWorkOrderActivity();
 
     /**
-     * Provide update work order activity update work order activity.
+     * Provides update work order activity.
      *
      * @return the update work order activity
      */
     UpdateWorkOrderActivity provideUpdateWorkOrderActivity();
 
     /**
-     * Provide close work order activity close work order activity.
+     * Provides close work order activity.
      *
      * @return the close work order activity
      */
     CloseWorkOrderActivity provideCloseWorkOrderActivity();
 
     /**
-     * Provide get manufacturers and models activity get manufacturers and models activity.
+     * Provides get manufacturers and models activity get manufacturers and models activity.
      *
      * @return the get manufacturers and models activity
      */
     GetManufacturersAndModelsActivity provideGetManufacturersAndModelsActivity();
 
     /**
-     * Provide get facilities and departments activity get facilities and departments activity.
+     * Provides get facilities and departments activity.
      *
      * @return the get facilities and departments activity
      */
