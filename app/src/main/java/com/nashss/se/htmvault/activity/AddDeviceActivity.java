f935561d49789c7c81e1dad1fbe463e9366a87ac
Aaron Kaye <kaye.aaron@gmail.com>
2023-06-19 17:18:00 -0400

    checkstyle corrections, added log statements, added code comments for clarification

--- app/src/main/java/com/nashss/se/htmvault/activity/AddDeviceActivity.java
+++ app/src/main/java/com/nashss/se/htmvault/activity/AddDeviceActivity.java
@@ -17,12 +17,14 @@ import com.nashss.se.htmvault.metrics.MetricsConstants;
 import com.nashss.se.htmvault.metrics.MetricsPublisher;
 import com.nashss.se.htmvault.models.ServiceStatus;
 import com.nashss.se.htmvault.utils.HTMVaultServiceUtils;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import javax.inject.Inject;
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
+
+import javax.inject.Inject;
 
 import static com.nashss.se.htmvault.utils.NullUtils.ifNull;
 
@@ -34,6 +36,14 @@ public class AddDeviceActivity {
     private final Logger log = LogManager.getLogger();
     private final MetricsPublisher metricsPublisher;
 
+    /**
+     * Instantiates a new Add device activity.
+     *
+     * @param deviceDao             the device dao
+     * @param manufacturerModelDao  the manufacturer model dao
+     * @param facilityDepartmentDao the facility department dao
+     * @param metricsPublisher      the metrics publisher
+     */
     @Inject
     public AddDeviceActivity(DeviceDao deviceDao, ManufacturerModelDao manufacturerModelDao,
                              FacilityDepartmentDao facilityDepartmentDao, MetricsPublisher metricsPublisher) {
@@ -43,6 +53,15 @@ public class AddDeviceActivity {
         this.metricsPublisher = metricsPublisher;
     }
 
+
+    /**
+     * Creates and adds the device per the request, with checks for valid input (i.e. the serial number is not null or
+     * blank and contains only the characters allowed, the manufacturer/model combination is a valid one, etc.).
+     * Throws corresponding exceptions, such as InvalidAttributeValueException, ManufacturerModelNotFoundException, etc.
+     *
+     * @param addDeviceRequest the add device request
+     * @return the add device result, containing a public device model, converted from the DDB device
+     */
     public AddDeviceResult handleRequest(final AddDeviceRequest addDeviceRequest) {
         log.info("Received AddDeviceRequest {}", addDeviceRequest);
 
@@ -60,13 +79,17 @@ public class AddDeviceActivity {
                 HTMVaultServiceUtils.ALPHA_NUMERIC_SPACE_OR_DASH);
         String model = addDeviceRequest.getModel();
         validateRequestAttribute("Model", model, HTMVaultServiceUtils.ALPHA_NUMERIC_SPACE_OR_DASH);
-        ManufacturerModel manufacturerModel = null;
+        ManufacturerModel manufacturerModel;
         try {
             manufacturerModel = manufacturerModelDao.getManufacturerModel(manufacturer, model);
         } catch (ManufacturerModelNotFoundException e) {
             metricsPublisher.addCount(MetricsConstants.ADDDEVICE_INVALIDATTRIBUTEVALUE_COUNT, 1);
+            log.info("The manufacturer/model combination specified ({}/{}) while attempting to add a new " +
+                    "device is not a valid combination", manufacturer, model);
             throw new InvalidAttributeValueException(e.getMessage());
         }
+        // if the manufacturer/model retrieved from the database has a null maintenance frequency,
+        // for our purposes, there is no maintenance required
         int requiredMaintenanceFrequencyInMonths =
                 ifNull(manufacturerModel.getRequiredMaintenanceFrequencyInMonths(), 0);
 
@@ -75,6 +98,8 @@ public class AddDeviceActivity {
             deviceDao.checkDevicePreviouslyAdded(manufacturerModel, serialNumber);
         } catch (DevicePreviouslyAddedException e) {
             metricsPublisher.addCount(MetricsConstants.ADDDEVICE_INVALIDATTRIBUTEVALUE_COUNT, 1);
+            log.info("An attempt was made to add a duplicate device with manufacturer/model {}/{} and " +
+                    "serial number {}", manufacturer, model, serialNumber);
             throw new InvalidAttributeValueException(e.getMessage());
         }
 
@@ -91,6 +116,8 @@ public class AddDeviceActivity {
             facilityDepartmentDao.getFacilityDepartment(facilityName, assignedDepartment);
         } catch (FacilityDepartmentNotFoundException e) {
             metricsPublisher.addCount(MetricsConstants.ADDDEVICE_INVALIDATTRIBUTEVALUE_COUNT, 1);
+            log.info("The facility/department combination specified ({}/{}) while attempting to add a new " +
+                    "device is not a valid combination", facilityName, assignedDepartment);
             throw new InvalidAttributeValueException(e.getMessage());
         }
 
@@ -101,12 +128,17 @@ public class AddDeviceActivity {
                 LocalDate manufactureDateParsed = new LocalDateConverter().unconvert(manufactureDate);
                 if (manufactureDateParsed.isAfter(LocalDate.now())) {
                     metricsPublisher.addCount(MetricsConstants.ADDDEVICE_INVALIDATTRIBUTEVALUE_COUNT, 1);
+                    log.info("The optional manufacture date provided while attempting to add a new device " +
+                            "is a future date ({})", manufactureDateParsed);
                     throw new InvalidAttributeValueException(String.format("Cannot provide a future manufacture date " +
                             "(%s)", manufactureDateParsed));
                 }
             } catch (DateTimeParseException e) {
                 metricsPublisher.addCount(MetricsConstants.ADDDEVICE_INVALIDATTRIBUTEVALUE_COUNT, 1);
-                throw new InvalidAttributeValueException("The date provided must be formatted as YYYY-MM-DD");
+                log.info("The optional manufacture date provided while attempting to add a new device is not " +
+                        "in the correct format of YYYY-MM-DD ({})", manufactureDate);
+                throw new InvalidAttributeValueException("The optional manufacture date, if provided, must be " +
+                        "formatted as YYYY-MM-DD");
             }
         }
 
@@ -138,9 +170,18 @@ public class AddDeviceActivity {
                 .build();
     }
 
+    /**
+     * Ensures a given attribute only contains the characters allowed.
+     *
+     * @param attributeName the actual attribute name (i.e. "serialNumber")
+     * @param attribute the value of the attribute (i.e. M1722, G-33143, etc.)
+     * @param validCharacterPattern a regex formatted string for pattern matching using the String matches method
+     */
     private void validateRequestAttribute(String attributeName, String attribute, String validCharacterPattern) {
         if (!HTMVaultServiceUtils.isValidString(attribute, validCharacterPattern)) {
             metricsPublisher.addCount(MetricsConstants.ADDDEVICE_INVALIDATTRIBUTEVALUE_COUNT, 1);
+            log.info("The {} provided ({}) while attempting to add a new device contained invalid " +
+                    "characters.", attributeName, attribute);
             throw new InvalidAttributeValueException(String.format("The %s provided (%s) contained invalid " +
                             "characters.", attributeName, attribute));
         }
