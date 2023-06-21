192b747ccea646c516fefbd33b02fa921b5d7100
Aaron Kaye <kaye.aaron@gmail.com>
2023-06-21 01:43:10 -0400

    added code comments, added javadoc descriptions - viewDevice.js

--- web/src/pages/viewDevice.js
+++ web/src/pages/viewDevice.js
@@ -9,32 +9,62 @@ import DataStore from "../util/DataStore";
 class ViewDevice extends BindingClass {
     constructor() {
         super();
-        this.bindClassMethods(['clientLoaded', 'submitRetire', 'submitReactivate', 'mount', 'addDeviceToPage', 'addWorkOrdersToPage', 'cancelUpdatesDevice', 'createWorkOrder', 'displayUpdateDeviceForm', 'submitDeviceUpdates', 'populateManufacturers', 'populateModels', 'populateFacilities', 'populateDepartments'], this);
+        // bind the class methods to keep track of state
+        this.bindClassMethods(['clientLoaded', 'submitRetire', 'submitReactivate', 'mount', 'addDeviceToPage',
+         'addWorkOrdersToPage', 'cancelUpdatesDevice', 'createWorkOrder', 'displayUpdateDeviceForm', 'submitDeviceUpdates',
+          'populateManufacturers', 'populateModels', 'populateFacilities', 'populateDepartments'], this);
+
+        // the datastore to store page information
         this.dataStore = new DataStore();
+
+        // the methods to run when an item in the datastore changes state
         this.dataStore.addChangeListener(this.addDeviceToPage);
         this.dataStore.addChangeListener(this.addWorkOrdersToPage);
+
+        // page header, including the home page link and the login/logout button
         this.header = new Header(this.dataStore);
+
         console.log("view device constructor");
     }
 
     /**
-     * Once the client is loaded, get the device metadata and work order list.
+     * Once the client is loaded, get the device and work order list metadata.
      */
     async clientLoaded() {
+        // obtain the control number from the url
         const urlParams = new URLSearchParams(window.location.search);
         const deviceId = urlParams.get('controlNumber');
+
+        // notify user that device is being loaded
         document.getElementById('control-number').innerText = "Loading Device ...";
+
+        // call the client to get the device
         const device = await this.client.getDevice(deviceId);
+
+        // if the device is in service, hide the button to reactivate the device
         if (device.serviceStatus == "IN_SERVICE") {
             document.getElementById('reactivate-device').classList.add('hidden');
+        // otherwise, the device is retired, so hide the buttons to retire and to update the device    
         } else {
             document.getElementById('retire-device').classList.add('hidden');
             document.getElementById('update-device').classList.add('hidden');
         }
+
+        // update the datastore with this device, which calls the method to add the device to the page,
+        // due to the change listener in the class constructor
         this.dataStore.set('device', device);
+
+        // obtain the sort order from the url
         const order = urlParams.get('order');
+
+        // notify user that the device's maintenance records are being loaded
         document.getElementById('work-orders').innerText = "(loading work orders...)";
+
+        // call the client to get the work orders, returning them in the order specified (i.e. descending or ascending)
         const workOrders = await this.client.getDeviceWorkOrders(deviceId, order);
+
+        // update the datastore with the work orders, which calls the method to add the work orders to
+        // the page, due to the change listener in the class constructor
         this.dataStore.set('workOrders', workOrders);
 
         const manufacturersAndModels = await this.client.getManufacturersAndModels();
@@ -44,21 +74,28 @@ class ViewDevice extends BindingClass {
         this.dataStore.set('facilitiesAndDepartments', facilitiesAndDepartments);
     }
 
+    /**
+     * Method for submitting updates when the corresponding button is clicked
+     */
     async submitDeviceUpdates(evt) {
         evt.preventDefault();
 
+        // an error message to unhide in the event a backend exception occurs and error message is returned
         const errorMessageDisplay = document.getElementById('update-error-message');
         errorMessageDisplay.innerText = ``;
         errorMessageDisplay.classList.add('hidden');
 
+        // a success message to unhide if the endpoint succeeds
         const successMessageDisplay = document.getElementById('success-message');
         successMessageDisplay.innerText = 'Device successfully updated.';
         successMessageDisplay.classList.add('hidden');
 
+        // inform the user that the update submission is being processed
         const updateButton = document.getElementById('update-device');
         const origButtonText = updateButton.innerText;
         updateButton.innerText = 'Updating...';
 
+        // obtain the input values being submitted for update in order to pass to the client
         const deviceControlNumber = document.getElementById('control-number').innerText;
         const deviceSerialNumber = document.getElementById('update-serial-number').value;
         const deviceManufacturer = document.getElementById('manufacturer-drop-down').value;
@@ -68,6 +105,7 @@ class ViewDevice extends BindingClass {
         const deviceManufactureDate = document.getElementById('update-manufacture-date').value;
         const deviceNotes = document.getElementById('update-notes').value;
 
+        // if the optional manufacture date is empty, set the value to null
         let manufactureDate;
         if (deviceManufactureDate.length < 1) {
             manufactureDate = null;
@@ -75,6 +113,7 @@ class ViewDevice extends BindingClass {
             manufactureDate = deviceManufactureDate;
         }
 
+        // if the optional device notes is empty, set the value to null
         let notes;
         if (deviceNotes.length < 1) {
             notes = null;
@@ -82,26 +121,37 @@ class ViewDevice extends BindingClass {
             notes = deviceNotes;
         }
 
-        const device = await this.client.updateDevice(deviceControlNumber, deviceSerialNumber, deviceManufacturer,
+        // the client call to update the device with the input provided
+        const device = await this.client.updateDevice(deviceControlNumber, deviceSerialNumber, deviceManufacturer, 
             deviceModel, deviceFacilityName, deviceAssignedDepartment, manufactureDate, notes, (error) => {
-            updateButton.innerText = origButtonText;
-            errorMessageDisplay.innerText = `Error: ${error.message}`;
-            errorMessageDisplay.classList.remove('hidden');
+                // reset the button to indicate the process is complete (when an error occurs)
+                updateButton.innerText = origButtonText;
+
+                // unhide the error message element
+                errorMessageDisplay.innerText = `Error: ${error.message}`;
+                errorMessageDisplay.classList.remove('hidden');
         });
 
+        // if the update succeeds, the update device form will be hidden and the device information will be
+        // unhidden, but the button text needs to be reset for the next time an attempt will potentially be made
+        // to update the device
         updateButton.innerText = origButtonText;
 
+        // if unsuccessful obtaining the device from the backend, return without updating the device in the datastore
         if (null == device) {
             return;
         }
 
+        // otherwise, update the device in the datastore
         this.dataStore.set('device', device);
 
+        // display the success message for 3-4 seconds
         successMessageDisplay.classList.remove('hidden');
         setTimeout(() => {
             successMessageDisplay.classList.add('hidden');
         }, 3500);
 
+        // hide the update device form; unhide the device record, work orders list, and the create new work order form
         const deviceRecordDiv = document.getElementById('device-record-div');
         const updateDeviceDiv = document.getElementById('update-device-div');
         const workOrdersDiv = document.getElementById('work-orders-div');
@@ -110,9 +160,12 @@ class ViewDevice extends BindingClass {
         deviceRecordDiv.classList.remove('hidden');
         workOrdersDiv.classList.remove('hidden');
         createWorkOrderDiv.classList.remove('hidden');
-
     }
 
+    /**
+     * Method for displaying the update device form, populated with the device's current values for the information
+     * that can potentially be edited
+     */
     async displayUpdateDeviceForm(evt) {
         evt.preventDefault();
 
@@ -331,25 +384,6 @@ class ViewDevice extends BindingClass {
     }
 
     /**
-     * Add the header to the page and load the HTMVaultClient.
-     */
-    mount() {
-        document.getElementById('retire-device').addEventListener('click', this.submitRetire);
-        document.getElementById('reactivate-device').addEventListener('click', this.submitReactivate);
-        document.getElementById('add-new-work-order').addEventListener('click', this.createWorkOrder);
-        document.getElementById('update-device').addEventListener('click', this.displayUpdateDeviceForm);
-        document.getElementById('submit-updates-device').addEventListener('click', this.submitDeviceUpdates);
-        document.getElementById('cancel-updates-device').addEventListener('click', this.cancelUpdatesDevice);
-        document.getElementById('manufacturer-drop-down').addEventListener('change', this.populateModels);
-        document.getElementById('facility-drop-down').addEventListener('change', this.populateDepartments);
-
-        this.header.addHeaderToPage();
-
-        this.client = new HTMVaultClient();
-        this.clientLoaded();
-    }
-
-    /**
      * When the device is updated in the datastore, update the device metadata on the page.
      */
     addDeviceToPage() {
@@ -424,8 +458,7 @@ class ViewDevice extends BindingClass {
     }
 
     /**
-     * Method to run when the add song playlist submit button is pressed. Call the MusicPlaylistService to add a song to the
-     * playlist.
+     * Method to run when the create work order submit button is pressed. Call the HTM Vault service to add the work order.
      */
     async createWorkOrder() {
 
@@ -466,6 +499,26 @@ class ViewDevice extends BindingClass {
         }
 
         document.getElementById('add-new-work-order').innerText = 'Create New Work Order';
+    }
+
+     /**
+     * Add the header to the page, load the HTMVaultClient, and initialize the page information
+     */
+    mount() {
+        // event listeners for button clicks and drop down selections
+        document.getElementById('retire-device').addEventListener('click', this.submitRetire);
+        document.getElementById('reactivate-device').addEventListener('click', this.submitReactivate);
+        document.getElementById('add-new-work-order').addEventListener('click', this.createWorkOrder);
+        document.getElementById('update-device').addEventListener('click', this.displayUpdateDeviceForm);
+        document.getElementById('submit-updates-device').addEventListener('click', this.submitDeviceUpdates);
+        document.getElementById('cancel-updates-device').addEventListener('click', this.cancelUpdatesDevice);
+        document.getElementById('manufacturer-drop-down').addEventListener('change', this.populateModels);
+        document.getElementById('facility-drop-down').addEventListener('change', this.populateDepartments);
+
+        this.header.addHeaderToPage();
+
+        this.client = new HTMVaultClient();
+        this.clientLoaded();
     }
 }
 
